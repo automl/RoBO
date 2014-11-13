5b7b3d276bd7eafdb76068c18e41e1b721e6f376
Jose Lykon <silenius@gmail.com>
2014-11-13 19:39:56 +0100

    sampling code in place, not entirely tested yet

--- robo/acquisition.py
+++ robo/acquisition.py
@@ -1,6 +1,6 @@
 from scipy.stats import norm
 import numpy as np
-        
+
 class PI(object):
     def __init__(self, model):
         self.model = model
@@ -22,7 +22,6 @@ class UCB(object):
         pass
 
 class Entropy(object):
-    # This function calls PI, EI etc and samples them (using their values)
     def __init__(self, model):
         self.model = model
     def __call__(self, X, Z=None, **kwargs):
@@ -32,6 +31,7 @@ class Entropy(object):
 
     # This method corresponds to the function SampleBeliefLocations in the original ES code
     # It is assumed that the GP data structure is a Python dictionary
+    # This function calls PI, EI etc and samples them (using their values)
     def sample_from_measure(self, gp, xmin, xmax, n_representers, BestGuesses, acquisition_fn):
 
         # If there are no prior observations, do uniform sampling
@@ -113,36 +113,73 @@ class Entropy(object):
 
     def slice_shrinkRank_nolog(self, xx, P, s0, transpose):
         # This function is equivalent to the similarly named function in the original ES code
-        if transpose: # Obs: the function is only called with transpose = True in the original code
-                      # also it appears that the case transpose = False leads to a runtime error
-            return xx.transpose()
-        else:
-            raise NotImplementedError
-        # D = xx.shape[0]
-        # f = P(xx.transpose())[0]
-        # logf = np.log(f)
-        # logy = np.log(np.random.uniform()) + logf
-        #
-        # theta = 0.95
-        #
-        # k = 0
-        # s = s0
-        # # the following two should be empty arrays, since they appears to grow dynamically I'll just use a list for now
-        # c = []
-        # J = []
-        #
-        # while True:
-        #     k += 1
+        if transpose:
+            xx = xx.transpose()
+
+        D = xx.shape[0]
+        f = P(xx.transpose())[0]
+        logf = np.log(f)
+        logy = np.log(np.random.uniform()) + logf
+
+        theta = 0.95
+
+        k = 0
+        s = s0
+        c = np.zeros((D,0))
+        J = np.zeros((0,0))
+
+        while True:
+            k += 1
+            c = np.append(c, self.projNullSpace(J, xx + s[k-1] * np.random.normal(size=(D,1))))
+            sx = np.divide(1., np.sum(np.divide(1., s)))
+            mx = np.dot(
+                sx,
+                np.sum(
+                    np.multiply(
+                        np.divide(1., s),
+                        np.subtract(c, xx)
+                    ),
+                    1))
+            xk = xx + self.projNullSpace(J, mx + np.multiply(sx, np.random.normal(size=(D,1))))
+
+            fk, dfk = P(xk.transpose())
+            logfk  = np.log(fk)
+            dlogfk = np.divide(dfk, fk)
+
+            if logfk > logy: # accept these values
+                xx = xk.transpose()
+                return xx
+            else: # shrink
+                g = self.projNullSpace(J, dlogfk)
+                if J.shape[1] < D - 1 & \
+                   np.dot(g.transpose(), dlogfk) > 0.5 * np.linalg.norm(g) * np.linalg.norm(dlogfk):
+                    J = np.append(J, np.divide(g, np.linalg.norm(g)))
+                    s[k] = s[k-1]
+                else:
+                    s[k] = np.multiply(theta, s[k-1])
+                    if s[k] < np.spacing(1):
+                        print 'bug found: contracted down to zero step size, still not accepted.\n'
+                    if transpose:
+                        xx = xx.transpose()
+                        return xx
+                    else:
+                        return xx
 
 
 
 
 
-    
+
 class EI(object):
     def __init__(self, model):
         self.model = model
-    def __call__(self, X, Z=None, **kwargs):
-        raise NotImplementedError
+    def __call__(self, x, par = 0.01, Z=None, **kwargs):
+        f_est = self.model.predict(x)
+        z = (f_est[0] - max(f_est[0]) - par) / f_est[1]
+        acqu = (f_est[0] - max(f_est[0]) - par) * norm.cdf(z) + f_est[1] * norm.pdf(z)
+        return acqu
+
+
+
     def model_changed(self):
         pass
