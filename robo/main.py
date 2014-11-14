dc488d496f250982fc411f1b2700394212139cb3
renumi <joel@renumi.de>
2014-11-14 14:05:44 +0100

    changing main

--- robo/main.py
+++ robo/main.py
@@ -9,61 +9,84 @@ import pylab as pb
 #pb.ion()
 from models import GPyModel 
 import numpy as np
-from test_functions import branin#, branin
+from test_functions import branin2, branin
 from acquisition import PI, UCB, Entropy
 from minimize import cma, DIRECT
 
 here = os.path.abspath(os.path.dirname(__file__))
 
-def bayesian_optimization(objective_fkt, acquisition_fkt, model, minimize_fkt, X_lower, X_upper,  maxN = 10, callback_fkt=lambda model, acq, i:True):
+#
+# Plotting Stuff.
+# It's really ugly until now, because it only fitts to 1D and to the branin function
+# where the second domension is 12
+#
+
+obj_samples = 700
+plot_min = -8
+plot_max = 19
+plotting_range = np.linspace(plot_min, plot_max, num=obj_samples)
+second_branin_arg = np.empty((obj_samples,))
+second_branin_arg.fill(12)
+branin_result = branin([plotting_range, second_branin_arg])
+def _plot_model(model, acquisition_fkt, objective_fkt, i):
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    model.m.plot(ax=ax, plot_limits=[plot_min, plot_max])
+    xlim_min, xlim_max, ylim_min, ylim_max =  ax.axis()
+    ax.set_ylim(min(np.min(branin_result), ylim_min), max(np.max(branin_result), ylim_max))
+    c1 = np.reshape(plotting_range, (obj_samples, 1))
+    c2 = acquisition_fkt(c1)
+    c2 = c2*50 / np.max(c2)
+    c2 = np.reshape(c2,(obj_samples,))
+    ax.plot(plotting_range,c2, 'r')
+    #ax.plot(plotting_range, branin_result, 'black')
+    fig.savefig("%s/tmp/np_%s.png"%(here, i), format='png')
+    fig.clf()
+    plt.close()
+    
+
+def bayesian_optimization(objective_fkt, acquisition_fkt, model, minimize_fkt, X_lower, X_upper,  maxN = 10):
     for i in xrange(maxN):
         acquisition_fkt.model_changed()
 
         new_x = minimize_fkt(acquisition_fkt, X_lower, X_upper)
         new_y = objective_fkt(new_x)
-        if not callback_fkt(model, acquisition_fkt, objective_fkt, i): 
-            break
+        _plot_model(model, acquisition_fkt, objective_fkt, i)
         model.update(new_x, new_y)
 
 
 
 
 def main():
-    obj_samples = 700
-    plot_min = -8
-    plot_max = 19
-    plotting_range = np.linspace(plot_min, plot_max, num=obj_samples)
-    second_branin_arg = np.empty((obj_samples,))
-    second_branin_arg.fill(12)
-    branin_result = branin([plotting_range, second_branin_arg])
-    def _plot_model(model, acquisition_fkt, objective_fkt, i):
-        return True
-        fig = plt.figure()
-        ax = fig.add_subplot(111)
-        model.m.plot(ax=ax, plot_limits=[plot_min, plot_max])
-        xlim_min, xlim_max, ylim_min, ylim_max =  ax.axis()
-        ax.set_ylim(min(np.min(branin_result), ylim_min), max(np.max(branin_result), ylim_max))
-        c1 = np.reshape(plotting_range, (obj_samples, 1))
-        c2 = acquisition_fkt(c1)
-        c2 = c2*50 / np.max(c2)
-        c2 = np.reshape(c2,(obj_samples,))
-        ax.plot(plotting_range,c2, 'r')
-        ax.plot(plotting_range, branin_result, 'black')
-        fig.savefig("%s/tmp/np_%s.png"%(here, i), format='png')
-        fig.clf()
-        plt.close()
-    kernel = GPy.kern.rbf(input_dim=2, variance=12.3, lengthscale=5.0)
-    X_lower = np.array([-8,-8])
-    X_upper = np.array([19, 19])
-    X = np.empty((1, 2))
+    #
+    # Dimension Space where the 
+    # objective function can be evaluated 
+    #
+    X_lower = np.array([-8])
+    X_upper = np.array([19])
+    #initialize the samples
+    X = np.empty((1, 1))
     Y = np.empty((1, 1))
+    #draw a random sample from the objective function in the
+    #dimension space 
     X[0,:] = [random.random() * (X_upper[0] - X_lower[0]) + X_lower[0]];
-    objective_fkt= branin
+    objective_fkt= branin2
     Y[0,:] = objective_fkt(X[0,:])
+    #
+    # Building up the model
+    #    
+    kernel = GPy.kern.rbf(input_dim=1, variance=12.3, lengthscale=5.0)
     model = GPyModel(kernel)
     model.train(X,Y)
-    acquisition_fkt = Entropy(model)
-    bayesian_optimization(objective_fkt, acquisition_fkt, model, DIRECT, X_lower, X_upper, maxN = 10, callback_fkt=_plot_model)
+    #
+    # creating an acquisition function
+    #
+    acquisition_fkt = UCB(model)
+    #
+    # start the main loop
+    #
+    bayesian_optimization(objective_fkt, acquisition_fkt, model, DIRECT, X_lower, X_upper, maxN = 10)
     
     
 
