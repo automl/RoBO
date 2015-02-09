bug fix PI: use deviation instead of variance and  pi test added

robo/acquisition/EI.py
@@ -27,8 +27,8 @@ class EI(object):
         m, v = self.model.predict(x)
         eta, _ = self.model.predict(np.array([self.model.getCurrentBestX()]))
         s = np.sqrt(v)
-        z = (eta - m - self.par) / s
-        f = (eta - m - self.par) * norm.cdf(z) + s * norm.pdf(z)
+        z = (eta - m) / s - self.par
+        f = (eta - m - self.par*s) * norm.cdf(z) + s * norm.pdf(z)
         if derivative:
             dmdx, ds2dx = self.model.m.predictive_gradients(x)
             dsdx = ds2dx / (2*s)

robo/acquisition/PI.py
@@ -19,29 +19,18 @@ class PI(object):
 
         alpha = np.linalg.solve(self.model.cK, np.linalg.solve(self.model.cK.transpose(), self.model.Y))
         dim = X.shape[1]
-        mean, var = self.model.predict(X, Z)
-        Y_star = self.model.getCurrentBest()
-        u = norm.cdf((Y_star - mean - self.par ) / var)
+        m, v = self.model.predict(X, Z)
+        eta = self.model.getCurrentBest()
+        s = np.sqrt(v)
+        z = (eta - m) / s - self.par
+        f = norm.cdf(z)
         if derivative:
-            # Derivative values:
-            # Derivative of kernel values:
-            dkxX = self.model.kernel.gradients_X(np.array([np.ones(len(self.model.X))]), self.model.X, X)
-            dkxx = self.model.kernel.gradients_X(np.array([np.ones(len(self.model.X))]), self.model.X)
-            # dmdx = derivative of the gaussian process mean function
-            dmdx = np.dot(dkxX.transpose(), alpha)
-            # dsdx = derivative of the gaussian process covariance function
-            dsdx = np.zeros((dim, 1))
-            for i in range(0, dim):
-                dsdx[i] = np.dot(0.5 / var, dkxx[0,dim-1] - 2 * np.dot(dkxX[:,dim-1].transpose(),
-                                                                       np.linalg.solve(self.model.cK,
-                                                                                       np.linalg.solve(self.model.cK.transpose(),
-                                                                                                       self.model.K[0,None].transpose()))))
-            # (-phi/s) * (dmdx + dsdx * z)
-            z = (Y_star - mean) / var
-            du = (- norm.pdf(z) / var) * (dmdx + dsdx * z)
-            return u, du
+            dmdx, ds2dx = self.model.m.predictive_gradients(X)
+            dsdx = ds2dx / (2*s)
+            df = -(- norm.pdf(z) / s) * (dmdx + dsdx * z)
+            return f, df
         else:
-            return u
+            return f
 
     def update(self, model):
         self.model = model

robo/models/GPyModel.py
@@ -80,10 +80,10 @@ class GPyModel(object):
             print "optimize finished"
             sys.stdout = stdout
 
-
-        index_min = np.argmin(self.Y)
+        self.observation_means = self.predict(self.X)[0]
+        index_min = np.argmin(self.observation_means)
         self.X_star = self.X[index_min]
-        self.Y_star = self.Y[index_min]
+        self.f_star = self.observation_means[index_min]
         self.K = self.kernel.K(X, X)
         try:
             self.cK = np.linalg.cholesky(self.K)
@@ -113,7 +113,7 @@ class GPyModel(object):
         return self.m.posterior_samples_f(X, size)
     
     def getCurrentBest(self):
-        return self.Y_star
+        return self.f_star
     
     def getCurrentBestX(self):
         return self.X_star

robo/test/test_pi.py
@@ -7,7 +7,7 @@ import numpy as np
 import random
 import GPy
 from robo.models import GPyModel
-from robo.acquisition import EI, Entropy
+from robo.acquisition import PI, Entropy
 from robo.visualization import Visualization
 import matplotlib.pyplot as plt
 class Dummy(object):
@@ -16,7 +16,7 @@ class Dummy(object):
 here = os.path.abspath(os.path.dirname(__file__))
 
 #@unittest.skip("skip first test\n")
-class EITestCase1(unittest.TestCase):
+class PITestCase1(unittest.TestCase):
     def setUp(self):
         self.x = np.array([[ 0.62971589], [ 0.63273273], [ 0.17867868], [ 0.17447447], [ 1.88558559]]);
         self.y = np.array([[-3.69925653], [-3.66221988], [-3.65560591], [-3.58907791], [-8.06925984]]);
@@ -31,28 +31,30 @@ class EITestCase1(unittest.TestCase):
     def test(self):
         X_upper = np.array([ 2.1])
         X_lower = np.array([-2.1])
-        ei_par_0 = EI(self.model, X_upper=X_upper, X_lower=X_lower,  par=0.0, derivative=True)
-        ei_par_1 = EI(self.model, X_upper=X_upper, X_lower=X_lower,  par=1.0, derivative=True)
-        ei_par_2 = EI(self.model, X_upper=X_upper, X_lower=X_lower,  par=2.0, derivative=True)
+        pi_par_0 = PI(self.model, X_upper=X_upper, X_lower=X_lower,  par=0.0, derivative=True)
+        pi_par_1 = PI(self.model, X_upper=X_upper, X_lower=X_lower,  par=1.0, derivative=True)
+        pi_par_2 = PI(self.model, X_upper=X_upper, X_lower=X_lower,  par=2.0, derivative=True)
         x_values = [0.62971589] + [2.1 * random.random() - 2.1 for i in range(10)]
-        out0 = np.array([ ei_par_0(np.array([[x]]), derivative=True) for x in x_values])
+        out0 = np.array([ pi_par_0(np.array([[x]]), derivative=True) for x in x_values])
         value0 = out0[:,0]
         derivative0 = out0[:,1]
         
-        out1 = np.array([ ei_par_1(np.array([[x]]), derivative=True) for x in x_values])
+        out1 = np.array([ pi_par_1(np.array([[x]]), derivative=True) for x in x_values])
         value1 = out1[:,0]
         derivative1 = out1[:,1]
         
-        out2 = np.array([ ei_par_2(np.array([[x]]), derivative=True) for x in x_values])
+        out2 = np.array([ pi_par_2(np.array([[x]]), derivative=True) for x in x_values])
         value2 = out2[:,0]
         derivative2 = out2[:,1]
+        
         assert(value0[0] <= 1e-5)
         assert(np.all(value0 >= value1))
         assert(np.all(value1 >= value2))
         assert(np.all(np.abs(derivative0) >= np.abs(derivative1)))
         assert(np.all(np.abs(derivative1) >= np.abs(derivative2)))
-        ei_par_0.update(self.noise_model)
-        out0_noise = np.array([ ei_par_0(np.array([[x]]), derivative=True) for x in x_values])
+        
+        pi_par_0.update(self.noise_model)
+        out0_noise = np.array([ pi_par_0(np.array([[x]]), derivative=True) for x in x_values])
         value0_noise = out0[:,0]
         derivative0_noise = out0[:,1]
         assert(np.all(np.abs(value0_noise) >= np.abs(value0)))
@@ -60,11 +62,11 @@ class EITestCase1(unittest.TestCase):
         bo_dummy = Dummy()
         bo_dummy.X_upper = X_upper
         bo_dummy.X_lower = X_lower
-        bo_dummy.acquisition_fkt = ei_par_1
+        bo_dummy.acquisition_fkt = pi_par_1
         bo_dummy.dims = 1
         bo_dummy.model = self.model
         bo_dummy.acquisition_fkt.update( bo_dummy.model)
-        dest_folder =here+"/tmp/test_ei/vis"
+        dest_folder =here+"/tmp/test_pi/vis"
         if dest_folder is not None:
             try:
                 os.makedirs(dest_folder)