start writing test_scripts, fixing derivative shapes

robo/acquisition/EI.py
@@ -1,4 +1,4 @@
-#encoding=utf8
+# encoding=utf8
 from scipy.stats import norm
 import scipy
 import numpy as np
@@ -8,19 +8,19 @@ from robo.acquisition.base import AcquisitionFunction
 class EI(AcquisitionFunction):
     
     long_name = "Expected Improvement" 
-    def __init__(self, model, X_lower, X_upper, par = 0.01,**kwargs):
+    def __init__(self, model, X_lower, X_upper, par=0.01, **kwargs):
         self.model = model
         self.par = par
         self.X_lower = X_lower
         self.X_upper = X_upper
 
-    def __call__(self, x, Z=None, derivative=False,  **kwargs):
+    def __call__(self, x, Z=None, derivative=False, **kwargs):
         if x.shape[0] > 1 :
             raise BayesianOptimizationError(BayesianOptimizationError.SINGLE_INPUT_ONLY, "EI is only for single x inputs")
         if np.any(x < self.X_lower) or np.any(x > self.X_upper):
             if derivative:
                 f = 0
-                df = np.zeros((x.shape[1],1))
+                df = np.zeros((1, x.shape[1]))
                 return np.array([[f]]), np.array([df])
             else:
                 return np.array([[0]])
@@ -35,19 +35,19 @@ class EI(AcquisitionFunction):
         if derivative:
             dmdx, ds2dx = self.model.predictive_gradients(x)
             dmdx = dmdx[0]
-            ds2dx = ds2dx[0][:,None]
-            dsdx = ds2dx / (2*s)
-            df = (-dmdx * norm.cdf(z) + (dsdx * norm.pdf(z)))
+            ds2dx = ds2dx[0][:, None]
+            dsdx = ds2dx / (2 * s)
+            df = (-dmdx * norm.cdf(z) + (dsdx * norm.pdf(z))).T
         if (f < 0).any():
             f[np.where(f < 0)] = 0.0
             if derivative:
                 df[np.where(f < 0), :] = np.zeros_like(x)
         if (f < 0).any():
             raise Exception
-        if len(f.shape)==1:
+        if len(f.shape) == 1:
             return_f = np.array([f])
         if derivative:
-            if len(df.shape)==3:
+            if len(df.shape) == 3:
                 return_df = df
             else:
                 return_df = np.array([df])

robo/acquisition/Entropy.py
@@ -15,7 +15,7 @@ eps = np.finfo(np.float32).eps
 here = os.path.abspath(os.path.dirname(__file__))
 class Entropy(AcquisitionFunction):
     long_name = "Information gain over p_min(x)" 
-    def __init__(self, model, X_lower, X_upper, Nb = 100, sampling_acquisition = None, sampling_acquisition_kw = {"par":0.0}, Np=200, loss_function=None, **kwargs):
+    def __init__(self, model, X_lower, X_upper, Nb=100, sampling_acquisition=None, sampling_acquisition_kw={"par":0.0}, Np=200, loss_function=None, **kwargs):
         self.model = model
         self.Nb = Nb 
         self.X_lower = np.array(X_lower)
@@ -32,33 +32,42 @@ class Entropy(AcquisitionFunction):
     
     def _get_most_probable_minimum(self):
         mi = np.argmax(self.logP)
-        xx = self.zb[mi,np.newaxis]
+        xx = self.zb[mi, np.newaxis]
         return xx
         
     def __call__(self, X, Z=None, derivative=False, **kwargs):
+        if X.shape[0] > 1 :
+            raise BayesianOptimizationError(BayesianOptimizationError.SINGLE_INPUT_ONLY, "Entropy is only for single x inputs")
+        if np.any(X < self.X_lower) or np.any(X > self.X_upper):
+            if derivative:
+                f = 0
+                df = np.zeros((1, X.shape[1]))
+                return np.array([[f]]), np.array([df])
+            else:
+                return np.array([[0]])
         return self.dh_fun(X, invertsign=True, derivative=derivative)
     
-    def sampling_acquisition_wrapper(self,x):
+    def sampling_acquisition_wrapper(self, x):
         return  self.sampling_acquisition(np.array([x]))[0]
     
     def update_representer_points(self):
         self.sampling_acquisition.update(self.model)
         
         restarts = np.zeros((self.Nb, self.D))    
-        restarts[0:self.Nb, ] = self.X_lower+ (self.X_upper-self.X_lower)* np.random.uniform( size = (self.Nb, self.D))
+        restarts[0:self.Nb, ] = self.X_lower + (self.X_upper - self.X_lower) * np.random.uniform(size=(self.Nb, self.D))
         
         sampler = emcee.EnsembleSampler(self.Nb, self.D, self.sampling_acquisition_wrapper)
         self.zb, self.lmb, _ = sampler.run_mcmc(restarts, 20)
         if len(self.zb.shape) == 1:
-            self.zb = self.zb[:,None]
+            self.zb = self.zb[:, None]
         if len(self.lmb.shape) == 1:
-            self.lmb = self.lmb[:,None]
+            self.lmb = self.lmb[:, None]
             
     def update(self, model):
         self.model = model
         self.update_representer_points()
         mu, var = self.model.predict(np.array(self.zb), full_cov=True)
-        self.logP,self.dlogPdMu,self.dlogPdSigma,self.dlogPdMudMu = self._joint_min(mu, var, with_derivatives=True)
+        self.logP, self.dlogPdMu, self.dlogPdSigma, self.dlogPdMudMu = self._joint_min(mu, var, with_derivatives=True)
         self.W = np.random.randn(1, self.Nb)
         self.logP = np.reshape(self.logP, (self.logP.shape[0], 1))
 
@@ -73,11 +82,11 @@ class Entropy(AcquisitionFunction):
         # Innovation function for covariance:
         dVdx = -Lx.dot(Lx.T)
         # The transpose operator is there to make the array indexing equivalent to matlab's
-        dVdx = dVdx[np.triu(np.ones((N,N))).T.astype(bool), np.newaxis]
+        dVdx = dVdx[np.triu(np.ones((N, N))).T.astype(bool), np.newaxis]
 
         dMM = dMdx.dot(dMdx.T)
         trterm = np.sum(np.sum(
-            np.multiply(self.dlogPdMudMu, np.reshape(dMM, (1, dMM.shape[0],dMM.shape[1]))),
+            np.multiply(self.dlogPdMudMu, np.reshape(dMM, (1, dMM.shape[0], dMM.shape[1]))),
             2), 1)[:, np.newaxis]
 
         # add a second dimension to the arrays if necessary:
@@ -95,7 +104,7 @@ class Entropy(AcquisitionFunction):
         s = _maxLPred + np.log(np.sum(np.exp(lPred - _maxLPred), axis=0))
         lselP = _maxLPred if np.any(np.isinf(s)) else s
         #
-        #lselP = np.log(np.sum(np.exp(lPred), 0))[np.newaxis,:]
+        # lselP = np.log(np.sum(np.exp(lPred), 0))[np.newaxis,:]
         # Normalise:
         lPred = np.subtract(lPred, lselP)
         
@@ -103,7 +112,7 @@ class Entropy(AcquisitionFunction):
         dH = np.mean(dHp)
         return dH
         
-    def dh_fun(self, x, invertsign = True, derivative=False):
+    def dh_fun(self, x, invertsign=True, derivative=False):
         
         if not (np.all(np.isfinite(self.lmb))):
             print self.zb[np.where(np.isinf(self.lmb))], self.lmb[np.where(np.isinf(self.lmb))]
@@ -119,34 +128,38 @@ class Entropy(AcquisitionFunction):
             ddHdx = np.zeros((x.shape[1], 1))
             return np.array([[dH]]), np.array([[ddHdx]])
         
-        if x.shape[0] > 1:
-            raise BayesianOptimizationError(BayesianOptimizationError.SINGLE_INPUT_ONLY, "dHdx_local is only for single x inputs")
         
         dH = self._dh_fun(x)
 
         if invertsign:
-            dH = - dH
+            dH = -dH
         if not np.isreal(dH):
             raise Exception("dH is not real")
         # Numerical derivative, renormalisation makes analytical derivatives unstable.
         e = 1.0e-5
         if derivative:
-            ddHdx = np.zeros((D,1))
+            ddHdx = np.zeros((1, D))
             for d in range(D):
-                ### First part:
+                # ## First part:
                 y = np.array(x)
-                y[d] += e
+                y[0, d] += e
                 dHy1 = self._dh_fun(y)
-                ### Second part:
+                # ## Second part:
                 y = np.array(x)
-                y[d] -= e
+                y[0, d] -= e
                 dHy2 = self._dh_fun(y)
                 
-                ddHdx[d] = np.divide((dHy1 - dHy2), 2*e)
+                ddHdx[0, d] = np.divide((dHy1 - dHy2), 2 * e)
                 if invertsign:
                     ddHdx = -ddHdx
             # endfor
-            return np.array([[dH]]), np.array([[ddHdx]])
+            
+            
+            if len(ddHdx.shape) == 3:
+                return_df = ddHdx
+            else:
+                return_df = np.array([ddHdx])
+            return np.array([[dH]]), return_df
         return np.array([[dH]])
 
     def _gp_innovation_local(self, x):
@@ -157,74 +170,74 @@ class Entropy(AcquisitionFunction):
         m, v = self.model.predict(x)
         s = np.sqrt(v)
         v_projected = self.model.predict_variance(x, self.zb)
-        Lx     = v_projected / s;
+        Lx = v_projected / s;
         dLxdx = None
         return Lx, dLxdx
     
-    def _joint_min(self, mu, var, with_derivatives= False, **kwargs):
+    def _joint_min(self, mu, var, with_derivatives=False, **kwargs):
         logP = np.zeros(mu.shape)
         D = mu.shape[0]
         if with_derivatives:
-            dlogPdMu    = np.zeros((D,D));
-            dlogPdSigma = np.zeros((D,0.5 * D * (D+1)));
-            dlogPdMudMu= np.zeros((D,D,D));
+            dlogPdMu = np.zeros((D, D));
+            dlogPdSigma = np.zeros((D, 0.5 * D * (D + 1)));
+            dlogPdMudMu = np.zeros((D, D, D));
         for i in xrange(mu.shape[0]):
             
-            #logP[k] ) self._min_faktor(mu, var, 0)
+            # logP[k] ) self._min_faktor(mu, var, 0)
             a = self._min_faktor(mu, var, i)
             
             logP[i] = a.next()            
             if with_derivatives:
-                dlogPdMu[i,:] = a.next().T
+                dlogPdMu[i, :] = a.next().T
                 dlogPdMudMu[i, :, :] = a.next()
-                dlogPdSigma[i,:] = a.next().T
+                dlogPdSigma[i, :] = a.next().T
             
         logP[np.isinf(logP)] = -500;    
-        #re-normalize at the end, to smooth out numerical imbalances:
-        logPold        = logP
-        Z     = np.sum(np.exp(logPold));
+        # re-normalize at the end, to smooth out numerical imbalances:
+        logPold = logP
+        Z = np.sum(np.exp(logPold));
         _maxLogP = np.max(logP)
         s = _maxLogP + np.log(np.sum(np.exp(logP - _maxLogP)))
         s = _maxLogP if np.isinf(s) else s
         
-        logP  = logP - s;
+        logP = logP - s;
         if not with_derivatives:
             return logP
         
-        dlogPdMuold    = dlogPdMu
+        dlogPdMuold = dlogPdMu
         dlogPdSigmaold = dlogPdSigma
         dlogPdMudMuold = dlogPdMudMu;
         # adjust derivatives, too. This is a bit tedious.
-        Zm    = sum(np.rot90((np.exp(logPold)*np.rot90(dlogPdMuold,1)),3)) / Z
-        Zs    = sum(np.rot90((np.exp(logPold)*np.rot90(dlogPdSigmaold,1)),3)) / Z 
+        Zm = sum(np.rot90((np.exp(logPold) * np.rot90(dlogPdMuold, 1)), 3)) / Z
+        Zs = sum(np.rot90((np.exp(logPold) * np.rot90(dlogPdSigmaold, 1)), 3)) / Z 
         
         
-        dlogPdMu    = dlogPdMuold-Zm
+        dlogPdMu = dlogPdMuold - Zm
         dlogPdSigma = dlogPdSigmaold - Zs
     
         ff = np.einsum('ki,kj->kij', dlogPdMuold, dlogPdMuold)
-        gg   = np.einsum('kij,k->ij',dlogPdMudMuold+ff,np.exp(logPold)) / Z;
-        Zij  = Zm.T * Zm;
-        adds = np.reshape(-gg+Zij,(1,D,D));
+        gg = np.einsum('kij,k->ij', dlogPdMudMuold + ff, np.exp(logPold)) / Z;
+        Zij = Zm.T * Zm;
+        adds = np.reshape(-gg + Zij, (1, D, D));
         dlogPdMudMu = dlogPdMudMuold + adds
-        return logP,dlogPdMu,dlogPdSigma,dlogPdMudMu
+        return logP, dlogPdMu, dlogPdSigma, dlogPdMudMu
             
-    def _min_faktor(self, Mu, Sigma, k, gamma = 1):
+    def _min_faktor(self, Mu, Sigma, k, gamma=1):
         D = Mu.shape[0]
-        logS = np.zeros((D-1,))
-        #mean time first moment
-        MP = np.zeros((D-1,))
+        logS = np.zeros((D - 1,))
+        # mean time first moment
+        MP = np.zeros((D - 1,))
         
-        #precision, second moment 
-        P = np.zeros((D-1,))
+        # precision, second moment 
+        P = np.zeros((D - 1,))
         
         M = np.copy(Mu)
         V = np.copy(Sigma)
         b = False
         for count in xrange(50):
             diff = 0
-            for i in range(D-1):
-                l = i if  i < k else i+1
+            for i in range(D - 1):
+                l = i if  i < k else i + 1
                 try:
                     M, V, P[i], MP[i], logS[i], d = self._lt_factor(k, l, M, V, MP[i], P[i], gamma)
                 except Exception, e:
@@ -239,113 +252,113 @@ class Entropy(AcquisitionFunction):
                 b = True
                 break;
         if np.isnan(d): 
-            logZ  = -np.Infinity;
+            logZ = -np.Infinity;
             yield logZ
-            dlogZdMu = np.zeros((D,1))
+            dlogZdMu = np.zeros((D, 1))
             yield dlogZdMu
             
-            dlogZdMudMu = np.zeros((D,D))
+            dlogZdMudMu = np.zeros((D, D))
             yield dlogZdMudMu
-            dlogZdSigma = np.zeros((0.5*(D*(D+1)),1))
+            dlogZdSigma = np.zeros((0.5 * (D * (D + 1)), 1))
             yield dlogZdSigma
-            mvmin = [Mu[k],Sigma[k,k]]
+            mvmin = [Mu[k], Sigma[k, k]]
             yield mvmin
         else:
-            #evaluate log Z:
+            # evaluate log Z:
             C = np.eye(D) / sq2 
-            C[k,:] = -1/sq2
+            C[k, :] = -1 / sq2
             C = np.delete(C, k, 1)
             
-            R       = np.sqrt(P.T) * C
-            r       = np.sum(MP.T * C, 1)
-            mp_not_zero = np.where(MP !=0)
+            R = np.sqrt(P.T) * C
+            r = np.sum(MP.T * C, 1)
+            mp_not_zero = np.where(MP != 0)
             mpm = MP[mp_not_zero] * MP[mp_not_zero] / P[mp_not_zero]
-            mpm     = sum(mpm);
+            mpm = sum(mpm);
             
-            s       = sum(logS);
-            IRSR    = (np.eye(D-1) + np.dot(np.dot(R.T , Sigma), R));
-            rSr     = np.dot(np.dot(r.T, Sigma) , r);
-            A =  np.dot(R,np.linalg.solve(IRSR,R.T)) 
+            s = sum(logS);
+            IRSR = (np.eye(D - 1) + np.dot(np.dot(R.T , Sigma), R));
+            rSr = np.dot(np.dot(r.T, Sigma) , r);
+            A = np.dot(R, np.linalg.solve(IRSR, R.T)) 
             
-            A       = 0.5 * (A.T + A) # ensure symmetry.
-            b       = (Mu + np.dot(Sigma,r));
-            Ab      = np.dot(A,b);
-            dts     = 2 * np.sum(np.log(np.diagonal(np.linalg.cholesky(IRSR))));
-            logZ    = 0.5 * (rSr - np.dot(b.T, Ab) - dts) + np.dot(Mu.T, r) + s - 0.5 * mpm;
+            A = 0.5 * (A.T + A)  # ensure symmetry.
+            b = (Mu + np.dot(Sigma, r));
+            Ab = np.dot(A, b);
+            dts = 2 * np.sum(np.log(np.diagonal(np.linalg.cholesky(IRSR))));
+            logZ = 0.5 * (rSr - np.dot(b.T, Ab) - dts) + np.dot(Mu.T, r) + s - 0.5 * mpm;
             yield logZ
             btA = np.dot(b.T, A)
             
-            dlogZdMu    = r - Ab
+            dlogZdMu = r - Ab
             yield dlogZdMu
             dlogZdMudMu = -A
             yield dlogZdMudMu
-            dlogZdSigma = -A - 2*np.outer(r,Ab.T) + np.outer(r,r.T) + np.outer(btA.T,Ab.T);
+            dlogZdSigma = -A - 2 * np.outer(r, Ab.T) + np.outer(r, r.T) + np.outer(btA.T, Ab.T);
             _dlogZdSigma = np.zeros_like(dlogZdSigma)
             np.fill_diagonal(_dlogZdSigma, np.diagonal(dlogZdSigma))
-            dlogZdSigma = 0.5*(dlogZdSigma+dlogZdSigma.T-_dlogZdSigma)
+            dlogZdSigma = 0.5 * (dlogZdSigma + dlogZdSigma.T - _dlogZdSigma)
             dlogZdSigma = np.rot90(dlogZdSigma, k=2)[np.triu_indices(D)][::-1];
             yield dlogZdSigma
             
     def _lt_factor(self, s, l, M, V, mp, p, gamma):
 
-        cVc = (V[l,l] - 2*V[s,l] + V[s,s])/ 2.0
-        Vc  = (V[:, l] - V [:, s]) / sq2
-        cM =  (M[l] - M[s])/ sq2
-        cVnic = np.max([cVc/(1-p * cVc), 0])
+        cVc = (V[l, l] - 2 * V[s, l] + V[s, s]) / 2.0
+        Vc = (V[:, l] - V [:, s]) / sq2
+        cM = (M[l] - M[s]) / sq2
+        cVnic = np.max([cVc / (1 - p * cVc), 0])
         cmni = cM + cVnic * (p * cM - mp)
-        z     = cmni / np.sqrt(cVnic);
-        e,lP,exit_flag = self._log_relative_gauss( z)
+        z = cmni / np.sqrt(cVnic);
+        e, lP, exit_flag = self._log_relative_gauss(z)
         if exit_flag == 0:
             alpha = e / np.sqrt(cVnic)
-            #beta  = alpha * (alpha + cmni / cVnic);
-            #r     = beta * cVnic / (1 - cVnic * beta);
-            beta  = alpha * (alpha * cVnic + cmni)
-            r     = beta / (1 - beta)
+            # beta  = alpha * (alpha + cmni / cVnic);
+            # r     = beta * cVnic / (1 - cVnic * beta);
+            beta = alpha * (alpha * cVnic + cmni)
+            r = beta / (1 - beta)
             # new message
-            pnew  = r / cVnic
-            mpnew = r * ( alpha + cmni / cVnic ) + alpha
+            pnew = r / cVnic
+            mpnew = r * (alpha + cmni / cVnic) + alpha
         
             # update terms
-            dp    = np.max([-p + eps,gamma * (pnew - p)]) # at worst, remove message
-            dmp   = np.max([-mp + eps,gamma * (mpnew- mp)])
-            d     = np.max([dmp,dp]) # for convergence measures
+            dp = np.max([-p + eps, gamma * (pnew - p)])  # at worst, remove message
+            dmp = np.max([-mp + eps, gamma * (mpnew - mp)])
+            d = np.max([dmp, dp])  # for convergence measures
         
-            pnew  = p  + dp;
+            pnew = p + dp;
             mpnew = mp + dmp;
-            #project out to marginal
-            Vnew  = V -  dp / (1 + dp * cVc) *np.outer(Vc,Vc)
+            # project out to marginal
+            Vnew = V - dp / (1 + dp * cVc) * np.outer(Vc, Vc)
             
-            Mnew  = M + (dmp - cM * dp) / (1 + dp * cVc) * Vc
+            Mnew = M + (dmp - cM * dp) / (1 + dp * cVc) * Vc
             if np.any(np.isnan(Vnew)): raise Exception("an error occurs while running expectation propagation in entropy search. Resulting variance contains NaN")
-            #% there is a problem here, when z is very large
-            logS  = lP - 0.5 * (np.log(beta) - np.log(pnew) - np.log(cVnic)) + (alpha * alpha) / (2*beta) * cVnic
+            # % there is a problem here, when z is very large
+            logS = lP - 0.5 * (np.log(beta) - np.log(pnew) - np.log(cVnic)) + (alpha * alpha) / (2 * beta) * cVnic
              
         elif exit_flag == -1:
             d = np.NAN
-            Mnew  = 0
-            Vnew  = 0;
-            pnew  = 0;    
+            Mnew = 0
+            Vnew = 0;
+            pnew = 0;    
             mpnew = 0;
-            logS  = -np.Infinity;
-            #raise Exception("-----")
+            logS = -np.Infinity;
+            # raise Exception("-----")
         elif exit_flag == 1:
-            d     = 0
+            d = 0
             # remove message from marginal:
             # new message
-            pnew  = 0 
+            pnew = 0 
             mpnew = 0
         
             # update terms
-            dp    = -p # at worst, remove message
-            dmp   = -mp
-            d     = max([dmp, dp]); # for convergence measures
+            dp = -p  # at worst, remove message
+            dmp = -mp
+            d = max([dmp, dp]);  # for convergence measures
         
             # project out to marginal
-            Vnew  = V - dp / (1 + dp * cVc) * (np.outer(Vc,Vc))
-            Mnew  = M + (dmp - cM * dp) / (1 + dp * cVc) * Vc;
+            Vnew = V - dp / (1 + dp * cVc) * (np.outer(Vc, Vc))
+            Mnew = M + (dmp - cM * dp) / (1 + dp * cVc) * Vc;
         
-            logS  = 0;
-        return Mnew,Vnew,pnew,mpnew,logS,d
+            logS = 0;
+        return Mnew, Vnew, pnew, mpnew, logS, d
     
     def _log_relative_gauss(self, z):
         if z < -6:
@@ -353,7 +366,7 @@ class Entropy(AcquisitionFunction):
         if z > 6:
             return 0, 0, 1 
         else:
-            logphi = -0.5 * ( z * z + l2p)
+            logphi = -0.5 * (z * z + l2p)
             logPhi = np.log(.5 * scipy.special.erfc(-z / sq2))
             e = np.exp(logphi - logPhi)
             return e, logPhi, 0
@@ -362,22 +375,22 @@ class Entropy(AcquisitionFunction):
         
         n = len(fig.axes)
         for i in range(n):
-            fig.axes[i].change_geometry(n+3, 1, i+1) 
-        ax = fig.add_subplot(n+3, 1, n+1)
-        bar_ax = fig.add_subplot(n+3, 1, n+2)
-        other_acq_ax = fig.add_subplot(n+3, 1, n+3)
+            fig.axes[i].change_geometry(n + 3, 1, i + 1) 
+        ax = fig.add_subplot(n + 3, 1, n + 1)
+        bar_ax = fig.add_subplot(n + 3, 1, n + 2)
+        other_acq_ax = fig.add_subplot(n + 3, 1, n + 3)
         plotting_range = np.linspace(minx, maxx, num=resolution)
-        acq_v =  np.array([ self(np.array([x]), derivative=True)[0][0] for x in plotting_range[:,np.newaxis] ])
+        acq_v = np.array([ self(np.array([x]), derivative=True)[0][0] for x in plotting_range[:, np.newaxis] ])
         ax.plot(plotting_range, acq_v, **plot_attr)
-        #ax.plot(self.plotting_range, acq_v[:,1])
+        # ax.plot(self.plotting_range, acq_v[:,1])
         zb = self.zb
         pmin = np.exp(self.logP)
         ax.set_xlim(minx, maxx)
-        bar_ax.bar(zb, pmin, width=(maxx - minx)/(2*zb.shape[0]), color="yellow")
+        bar_ax.bar(zb, pmin, width=(maxx - minx) / (2 * zb.shape[0]), color="yellow")
         bar_ax.set_xlim(minx, maxx)
         
         
         other_acq_ax.set_xlim(minx, maxx)
-        self.sampling_acquisition.plot(fig, minx, maxx, plot_attr={"color":"orange"})#, logscale=True)
+        self.sampling_acquisition.plot(fig, minx, maxx, plot_attr={"color":"orange"})  # , logscale=True)
         ax.set_title(str(self))
         return ax

robo/acquisition/PI.py
@@ -13,11 +13,11 @@ class PI(AcquisitionFunction):
 
     def __call__(self, X, Z=None, derivative=False, **kwargs):
         if X.shape[0] > 1 :
-            raise BayesianOptimizationError(BayesianOptimizationError.SINGLE_INPUT_ONLY, "EI is only for single x inputs")
+            raise BayesianOptimizationError(BayesianOptimizationError.SINGLE_INPUT_ONLY, "PI is only for single x inputs")
         if np.any(X < self.X_lower) or np.any(X > self.X_upper):
             if derivative:
                 f = 0
-                df = np.zeros((x.shape[1],1))
+                df = np.zeros((1, X.shape[1]))
                 return np.array([[f]]), np.array([df])
             else:
                 return np.array([[0]])
@@ -32,13 +32,14 @@ class PI(AcquisitionFunction):
         if derivative:
             dmdx, ds2dx = self.model.predictive_gradients(X)
             dmdx = dmdx[0]
-            ds2dx = ds2dx[0][:,None]
-            dsdx = ds2dx / (2*s)
-            df = -(- norm.pdf(z) / s) * (dmdx + dsdx * z)
-        if len(f.shape)==1:
+            ds2dx = ds2dx[0][:, None]
+            dsdx = ds2dx / (2 * s)
+            df = (-(-norm.pdf(z) / s) * (dmdx + dsdx * z)).T
+            
+        if len(f.shape) == 1:
             return_f = np.array([f])
         if derivative:
-            if len(df.shape)==3:
+            if len(df.shape) == 3:
                 return_df = df
             else:
                 return_df = np.array([df])

robo/maximize.py
@@ -23,13 +23,13 @@ try:
     def _cma_fkt_wrapper(acq_f, derivative=False):
        def _l(x, *args, **kwargs):
            x = np.array([x])
-           return -acq_f(x, derivative=derivative,*args, **kwargs)
+           return -acq_f(x, derivative=derivative, *args, **kwargs)
        return _l
     def cma(acquisition_fkt, X_lower, X_upper):
-        #stdout = sys.stdout
-        #sys.stdout = StringIO.StringIO()
-        x = _cma.fmin(_cma_fkt_wrapper(acquisition_fkt), (X_upper + X_lower)*0.5, 0.6, options={"bounds":[X_lower, X_upper], "verbose":-1, "verb_log":sys.maxint})[0]
-        #sys.stdout = stdout
+        # stdout = sys.stdout
+        # sys.stdout = StringIO.StringIO()
+        x = _cma.fmin(_cma_fkt_wrapper(acquisition_fkt), (X_upper + X_lower) * 0.5, 0.6, options={"bounds":[X_lower, X_upper], "verbose":-1, "verb_log":sys.maxint})[0]
+        # sys.stdout = stdout
         return np.array([x])
 
 except Exception, e:
@@ -38,7 +38,7 @@ except Exception, e:
 
 def grid_search(acquisition_fkt, X_lower, X_upper, resolution=1000):
     from numpy import linspace, array
-    if  X_lower.shape[0] >1 :
+    if  X_lower.shape[0] > 1 :
         raise RuntimeError("grid search works for 1D only")
     x = linspace(X_lower[0], X_upper[0], resolution).reshape((resolution, 1, 1))
     # y = array(map(acquisition_fkt, x))
@@ -49,17 +49,15 @@ def grid_search(acquisition_fkt, X_lower, X_upper, resolution=1000):
     x_star = x[y.argmax()]
     return x_star
 
-
 def _scipy_optimizer_fkt_wrapper(acq_f, derivative=True):
     def _l(x, *args, **kwargs):
         x = np.array([x])
         if np.any(np.isnan(x)):
-            print x
             raise Exception("oO")
-        a = acq_f(x, derivative=derivative,*args, **kwargs)
+        a = acq_f(x, derivative=derivative, *args, **kwargs)
         
         if derivative:
-            return -a[0][0], -a[1][0][:,0]
+            return -a[0][0], -a[1][0][0, :]
            
         else:
             return -a[0]
@@ -72,7 +70,7 @@ def stochastic_local_search(acquisition_fkt, X_lower, X_upper, Ne=20):
         xx = np.add(np.multiply((X_lower - X_upper), np.random.uniform(size=(1, X_lower.shape[0]))), X_lower)
     def fun_p(x):
         acq_v = acquisition_fkt(np.array([x]))[0]
-        log_acq_v = np.log(acq_v) if  acq_v>0 else -np.inf
+        log_acq_v = np.log(acq_v) if  acq_v > 0 else -np.inf
         
         return log_acq_v
     sc_fun = _scipy_optimizer_fkt_wrapper(acquisition_fkt)
@@ -81,7 +79,7 @@ def stochastic_local_search(acquisition_fkt, X_lower, X_upper, Ne=20):
     Xstart = np.zeros((Ne, D))
         
     restarts = np.zeros((Ne, D))    
-    restarts[0:Ne, ] = X_lower+ (X_upper-X_lower)* np.random.uniform( size = (Ne, D))
+    restarts[0:Ne, ] = X_lower + (X_upper - X_lower) * np.random.uniform(size=(Ne, D))
     sampler = emcee.EnsembleSampler(Ne, D, fun_p)
     Xstart, logYstart, _ = sampler.run_mcmc(restarts, 20)
     search_cons = []
@@ -96,15 +94,14 @@ def stochastic_local_search(acquisition_fkt, X_lower, X_upper, Ne=20):
     minima = []
     jacobian = True
     i = 0 
-    print restarts, search_cons
     while i < Ne:
         try:
             minima.append(scipy.optimize.minimize(
-               fun=sc_fun, x0=Xstart[i,np.newaxis], jac=jacobian, method='L-BFGS-B', constraints=search_cons,
+               fun=sc_fun, x0=Xstart[i, np.newaxis], jac=jacobian, method='L-BFGS-B', constraints=search_cons,
                options={'ftol':np.spacing(1), 'maxiter':20}
             ))
             i += 1
-        #no derivatives
+        # no derivatives
         except BayesianOptimizationError, e:
             if e.errno == BayesianOptimizationError.NO_DERIVATIVE:
                 jacobian = False