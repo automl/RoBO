ed06a1c98d0df5782a750b61b22f1589cbfbcea7
Jose Luis Licon <liconj@informatik.uni-freiburg.de>
2015-03-24 15:58:54 +0100

    code comments

--- robo/loss_functions.py
+++ robo/loss_functions.py
@@ -1,7 +1,23 @@
-#import math
+# encoding=utf8
+
+"""
+This module contains the loss functions used in the calculation of the expected information gain.
+For the moment only the logloss function is implemented.
+
+    .. method:: __init__(model, X_lower, X_upper, Nb=100, sampling_acquisition=None, sampling_acquisition_kw={"par":0.0}, Np=200, loss_function=None, **kwargs)
+
+        :param logP: Log-probability values.
+        :param lmb: Log values of acquisition function at belief points.
+        :param lPred: Log of the predictive distribution
+        :param args: Additional parameters
+        :return:
+
+"""
+
 import numpy as np
 
 def logLoss(logP, lmb, lPred, *args):
+
     H =   - np.sum(np.multiply(np.exp(logP), (logP + lmb))) # current entropy
     
     
