417e97fcdc0972e84d9e7e9283936ebfaf8c1baa
Aaron Klein <kleinaa@cs.uni-freiburg.de>
2015-06-12 18:15:05 +0200

    add more code

--- robo/acquisition/EnvEntropySearch.py
+++ robo/acquisition/EnvEntropySearch.py
@@ -7,7 +7,8 @@ Created on Jun 8, 2015
 import emcee
 import numpy as np
 
-from Entropy import Entropy
+from robo.acquisition.LogEI import LogEI
+from robo.acquisition.Entropy import Entropy
 
 
 class EnvEntropySearch(Entropy):
@@ -15,33 +16,47 @@ class EnvEntropySearch(Entropy):
     classdocs
     '''
 
-    def __init__(self, model, cost_model, n_representer, n_hals_vals, n_func_samples, **kwargs):
-        '''
-        Constructor
-        '''
+    def __init__(self, model, cost_model, n_representer, n_hals_vals, n_func_samples, X_lower, X_upper, compute_incumbent, **kwargs):
+
+        self.model = model
+        self.cost_model = cost_model
+        self.n_representers = n_representer
+        self.n_func_samples = n_func_samples
+        self.n_hals_vals = n_hals_vals
+
+        self.proposal_measure = LogEI(self.model, X_lower, X_upper, compute_incumbent)
+
+    def update(self, model, cost_model):
+
         self.model = model
         self.cost_model = cost_model
 
+        self.proposal_measure.update(model)
+
         # Sample representer points
+        #TODO: project representer points
+        self.representers = self._sample_representers(self.proposal_measure, self.n_representers, n_dim)
+
+        # Compute log propsal vals
+        self.log_proposal_vals = np.array([self.proposal_measure(self.representers[i]) for i in xrange(self.n_representers)])
 
         # Compute kl divergence between current pmin and the uniform distribution
-
-
-    def update(self, model):
-        """
-        """
-        self.model = model
+        pmin = self._compute_pmin(self.model, self.representers)
+        self.current_k = self._compute_kl_divergence(pmin, self.log_proposal_vals)
 
     def __call__(self, X, derivative=False):
-        """
-        """
+
         # Compute kl divergence between the updated pmin and the uniform distribution
+        pmin = self._compute_pmin(self.model, self.representers)
 
         # Predict the costs for this configuration
-
+        cost = self.cost_model.predict(X)
         # Compute acquisition value
+        kl = self._compute_kl_divergence(pmin, self.log_proposal_vals)
 
-        raise NotImplementedError()
+        acquisition_value = (self.current_kl - kl) / cost
+
+        return acquisition_value
 
     def _sample_representers(self, proposal_measure, n_representers, n_dim, burnin_steps=100, mcmc_steps=100):
 
