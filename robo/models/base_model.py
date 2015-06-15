ec0f76116de5ab4f3512fd70e8f2cd0eec32e976
Aaron Klein <kleinaa@cs.uni-freiburg.de>
2015-06-15 10:57:24 +0200

    update some documentations

--- robo/models/base_model.py
+++ robo/models/base_model.py
@@ -1,17 +1,29 @@
-import numpy as np
 
 
 class BaseModel(object):
     """
-     Abstract base class for all models 
+     Abstract base class for all models
     """
     def __init__(self, *args, **kwargs):
         self.X = None
         self.y = None
 
     def train(self, X, y):
+        """
+            Trains the model on the provided data.
+            :param X: Input datapoints. The dimensionality of X is (N, D), with N as the number of points and D is the number of features.
+            :type X: np.ndarray (N, D)
+            :param Y: The corresponding target values. The dimensionality of Y is (N), where N has to match the number of points of X
+            :type Y: np.ndarray (N)
+        """
         self.X = X
         self.y = y
 
     def predict(self, X):
+        """
+            Predicts for a given X matrix the target values
+            :param X: Test datapoints. The dimensionality of X is (N, D), with N as the number of points and D is the number of features.
+            :type X: np.ndarray (N, D)
+            :return The mean and variance of the testdatapoints.
+        """
         raise NotImplementedError()
