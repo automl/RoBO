d33ad7fa0d6aa6710b5fff33419b0a2b77d62388
Aaron Klein <kleinaa@cs.uni-freiburg.de>
2015-07-03 11:09:03 +0200

    improving fmin and add an example

--- robo/fmin.py
+++ robo/fmin.py
@@ -30,6 +30,7 @@ def fmin(objective_fkt, X_lower, X_upper, num_iterations=30, model="GPy", maximi
         k = GPy.kern.RBF(input_dim=dims)
     else:
         print "ERROR: Kernel %s is not a valid kernel!" % (kernel)
+        return None
 
     if model == "GPy":
         m = GPyModel(k, optimize=True, noise_variance=1e-4, num_restarts=10)
@@ -37,6 +38,7 @@ def fmin(objective_fkt, X_lower, X_upper, num_iterations=30, model="GPy", maximi
         m = GPyModelMCMC(k, optimize=True, noise_variance=1e-4, num_restarts=10)
     else:
         print "ERROR: %s is not a valid model!" % (model)
+        return None
 
     if acquisition_fkt == "EI":
         a = EI(m, X_upper=X_upper, X_lower=X_lower, compute_incumbent=compute_incumbent, par=0.1)
@@ -50,21 +52,23 @@ def fmin(objective_fkt, X_lower, X_upper, num_iterations=30, model="GPy", maximi
         a = EntropyMC(model, X_upper=X_upper, X_lower=X_lower, compute_incumbent=compute_incumbent, par=0.1)
     else:
         print "ERROR: %s is not a valid acquisition function!" % (acquisition_fkt)
+        return None
 
     if maximizer == "cmaes":
-        max = cmaes
+        max_fkt = cmaes
     elif maximizer == "direct":
-        max = direct
+        max_fkt = direct
     elif maximizer == "stochastic_local_search":
-        max = stochastic_local_search
+        max_fkt = stochastic_local_search
     elif maximizer == "grid_search":
-        max = grid_search
+        max_fkt = grid_search
     else:
         print "ERROR: %s is not a valid function to maximize the acquisition function!" % (acquisition_fkt)
+        return None
 
     bo = BayesianOptimization(acquisition_fkt=a,
                           model=m,
-                          maximize_fkt=max,
+                          maximize_fkt=max_fkt,
                           X_lower=X_lower,
                           X_upper=X_upper,
                           dims=dims,
