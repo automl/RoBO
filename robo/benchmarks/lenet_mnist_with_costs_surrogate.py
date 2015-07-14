3b5f9480bc3142c6ee34cae2a5c1ba273d597473
aaron <kleinaa@informatik.uni-freiburg.de>
2015-07-14 18:41:58 +0200

    remove benchmarks

--- robo/benchmarks/lenet_mnist_with_costs_surrogate.py
+++ robo/benchmarks/lenet_mnist_with_costs_surrogate.py
@@ -11,9 +11,9 @@ from sklearn.ensemble import RandomForestRegressor
 from ParameterConfigSpace.config_space import ConfigSpace
 
 
-def lenet_mnist_surrogate(x):
-    X = np.load("robo/benchmarks/data_lenet_mnist_surrogate/data.npy")
-    y = np.load("robo/benchmarks/data_lenet_mnist_surrogate/targets.npy")
+def lenet_mnist_with_cost_surrogate(x):
+    X = np.load("robo/benchmarks/data_lenet_mnist_with_cost_surrogate/data.npy")
+    y = np.load("robo/benchmarks/data_lenet_mnist_with_cost_surrogate/targets.npy")
 
     rf = RandomForestRegressor()
     rf.fit(X, y)
@@ -21,11 +21,19 @@ def lenet_mnist_surrogate(x):
     return y_hat
 
 
-def get_lenet_mnist_surrogate_bounds():
-    cs = ConfigSpace("robo/benchmarks/data_lenet_mnist_surrogate/param.pcs")
-    n_dims = len(cs.parameters)
-    X_lower = np.zeros([n_dims])
-    X_upper = np.zeros([n_dims])
-    for i, name in enumerate(cs.get_parameter_names()):
-        X_lower[i], X_upper[i] = cs.parameters[name].values
-    return X_lower, X_upper, n_dims
+def get_lenet_mnist_with_cost_surrogate_bounds():
+    #cs = ConfigSpace("robo/benchmarks/data_lenet_mnist_with_cost_surrogate/param.pcs")
+    #n_dims = len(cs.parameters)
+    #X_lower = np.zeros([n_dims])
+    #X_upper = np.zeros([n_dims])
+    X = np.load("robo/benchmarks/data_lenet_mnist_with_cost_surrogate/data.npy")
+    n_dims = 8
+    X_lower = np.min(X, axis=0)
+    X_upper = np.max(X, axis=0)
+    is_env = np.zeros([n_dims])
+    #for i, name in enumerate(cs.get_parameter_names()):
+    #    if 'env' in name:
+    #        is_env[i] = 1
+    #    X_lower[i], X_upper[i] = cs.parameters[name].values
+
+    return X_lower, X_upper, n_dims, is_env
