b37522d38486a074a385c59ae82c96390d7f39f0
numairmansur <numair.mansur@gmail.com>
2015-09-04 15:35:34 +0200

    change8

--- robo/visualization/plotting.py
+++ robo/visualization/plotting.py
@@ -10,6 +10,26 @@ import numpy as np
 
 
 def plot_model(model, X_lower, X_upper, ax, resolution=0.1,mean_color='b',uncertainity_color='blue',label="Model",std_scale=3,plot_mean=True):
+    ''' Plots the model on the ax object passed to it
+
+    Args:
+        model (object): Model of the objective funciton
+        X_lower (np.array): Upper bound of the input space
+        X_upper (np.array): Lower bound of the input space
+        ax (object): subplot for the model and the objective funciton
+        resolution (float): resolution for the subplot
+        mean_color (string): Color of the prosterior mean
+        uncertainity_color (string): Color of the model
+        label (string): Label string
+        std_scale (int): Standard Deviation Scale
+        plot_mean (bool): Bool flag, Plot the mean curve if value is True 
+
+    Returns:
+        ax (object) : subplot for the model and the objective funciton
+
+    '''
+
+
     X = np.arange(X_lower[0], X_upper[0], resolution)
 
     mean = np.zeros([X.shape[0]])
@@ -25,6 +45,23 @@ def plot_model(model, X_lower, X_upper, ax, resolution=0.1,mean_color='b',uncert
 
 
 def plot_objective_function(objective_function, X_lower, X_upper, X, Y, ax, resolution=0.1,color='black',label='ObjectiveFunction',maximizer_flag=True):
+    ''' Plots the objective_function on the ax object passed to it
+
+    Args:
+        objective_function ():
+        X_lower ():
+        X_upper ():
+        X ():
+        Y ():
+        ax ():
+        resolution ():
+        color ():
+        label ():
+        maximizer_flag ():
+    Returns:
+        ax ():
+
+    '''
     grid = np.arange(X_lower[0], X_upper[0], resolution)
 
     grid_values = np.zeros([grid.shape[0]])
@@ -40,6 +77,21 @@ def plot_objective_function(objective_function, X_lower, X_upper, X, Y, ax, reso
 
 
 def plot_acquisition_function(acquisition_function, X_lower, X_upper,X, ax, resolution=0.1,label="AcquisitionFunction", maximizer_flag = True):
+    ''' Plots the acquisition_function on the ax object passed to it
+
+    Args:
+        acquisition_function ():
+        X_lower ():
+        X_upper ():
+        X ():
+        ax ():
+        resolution ():
+        label ():
+        maximizer_flag ():
+    Returns:
+        ax ():
+
+    '''
     grid = np.arange(X_lower[0], X_upper[0], resolution)
 
     grid_values = np.zeros([grid.shape[0]])
