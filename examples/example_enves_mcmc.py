503706d13aada97d5f741c677b194857c42ab89f
Aaron Klein <kleinaa@cs.uni-freiburg.de>
2015-10-28 11:14:10 +0100

    fix examples

--- examples/example_enves_mcmc.py
+++ examples/example_enves_mcmc.py
@@ -12,48 +12,93 @@ import george
 
 from robo.models.gaussian_process_mcmc import GaussianProcessMCMC
 from robo.models.gaussian_process import GaussianProcess
-from robo.acquisition.env_entropy_search import EnvEntropySearch
+from robo.acquisition.environment_entropy import EnvironmentEntropy
 from robo.acquisition.integrated_acquisition import IntegratedAcquisition
 from robo.maximizers.direct import Direct
-from robo.recommendation.incumbent import compute_incumbent
 from robo.recommendation.optimize_posterior import env_optimize_posterior_mean_and_std
 from robo.task.env_branin import EnvBranin
 from robo.solver.environment_search import EnvironmentSearch
+from robo.priors import default_priors
+from robo.priors.base_prior import BasePrior
 
 
-def lnprior(x):
-    if np.any((-10 > x) + (x > 10)):
-        return -np.inf 
-    return 0
+class Prior(BasePrior):
 
+    def __init__(self, n_dims):
+
+        # The number of hyperparameters
+        self.n_dims = n_dims
+
+        # Prior for the Matern52 lengthscales
+        self.tophat = default_priors.TophatPrior(-2, 2)
+
+        # Prior for the covariance amplitude
+        self.ln_prior = default_priors.LognormalPrior(mean=0.0, sigma=1.0)
+
+        # Prior for the noise
+        self.horseshoe = default_priors.HorseshoePrior(scale=0.1)
+
+    def lnprob(self, theta):
+        lp = 0
+        # Covariance amplitude
+        lp += self.ln_prior.lnprob(theta[0])
+        # Lengthscales
+        lp += self.tophat.lnprob(theta[1:-1])
+        # Noise
+        lp += self.horseshoe.lnprob(theta[-1])
+
+        return lp
+
+    def sample_from_prior(self, n_samples):
+        p0 = np.zeros([n_samples, self.n_dims])
+        # Covariance amplitude
+        p0[:, 0] = self.ln_prior.sample_from_prior(n_samples)
+        # Lengthscales
+        ls_sample = np.array([self.tophat.sample_from_prior(n_samples)
+                              for _ in range(1, (self.n_dims - 1))]).T
+        p0[:, 1:(self.n_dims - 1)] = ls_sample
+        # Noise
+        p0[:, -1] = self.horseshoe.sample_from_prior(n_samples)
+
+        return p0
+
+# Adapted branin function with additional system size
 branin = EnvBranin()
 
+# Define the kernel + prior for modeling the objective function
 noise = 1.0
 cov_amp = 2
 exp_kernel = george.kernels.ExpSquaredKernel([1.0, 1.0, 1.0], ndim=3)
 noise_kernel = george.kernels.WhiteKernel(noise, ndim=3)
 kernel = cov_amp * (exp_kernel + noise_kernel)
 
-model = GaussianProcessMCMC(kernel, lnprior=lnprior, chain_length=100, burnin_steps=200, n_hypers=20)
+prior = Prior(len(kernel))
 
+model = GaussianProcessMCMC(kernel, prior=prior, chain_length=100,
+                            burnin_steps=200, n_hypers=20)
 
+# Define the kernel + prior for modeling the cost function
 cost_noise = 1.0
 cost_cov_amp = 2
 cost_exp_kernel = george.kernels.ExpSquaredKernel([1.0, 1.0, 1.0], ndim=3)
 cost_noise_kernel = george.kernels.WhiteKernel(cost_noise, ndim=3)
 cost_kernel = cost_cov_amp * (cost_exp_kernel + cost_noise_kernel)
 
-cost_model = GaussianProcessMCMC(cost_kernel, lnprior=lnprior)
+cost_prior = Prior(len(cost_kernel))
 
-es = EnvEntropySearch(model, cost_model, branin.X_lower, branin.X_upper, env_optimize_posterior_mean_and_std, branin.is_env, 50)
+cost_model = GaussianProcessMCMC(cost_kernel, prior=cost_prior)
+
+# Inititalize the BO ingredients
+es = EnvironmentEntropy(model, cost_model, branin.X_lower, branin.X_upper,
+                        env_optimize_posterior_mean_and_std, branin.is_env, 50)
 acquisition_func = IntegratedAcquisition(model, es, cost_model)
-        
-
 maximizer = Direct(acquisition_func, branin.X_lower, branin.X_upper)
+
 bo = EnvironmentSearch(acquisition_func=acquisition_func,
                   model=model,
                   cost_model=cost_model,
                   maximize_func=maximizer,
                   task=branin,
-                  synthetic_func=True)
-bo.run(10)
+                  synthetic_func=True,
+                  n_init_points=2)
+bo.run(20)
