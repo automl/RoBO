98fd920dbedc858e2f7dcbd6a5851137e385c928
Aaron Klein <kleinaa@cs.uni-freiburg.de>
2016-02-11 13:24:18 +0100

    add normal prior

--- robo/priors/base_prior.py
+++ robo/priors/base_prior.py
@@ -218,7 +218,10 @@ class HorseshoePrior(BasePrior):
         (D) np.array
             The gradient of the prior at theta.
         """
-        pass
+        a = -(6 * self.scale ** 2)
+        b = (3 * self.scale ** 2 + np.exp(2 * theta))
+        b *= np.log(3 * self.scale ** 2 * np.exp(- 2 * theta) + 1)
+        return a / b
 
 
 class LognormalPrior(BasePrior):
@@ -230,9 +233,9 @@ class LognormalPrior(BasePrior):
         ----------
         sigma: float
             Specifies the standard deviation of the normal
-        distribution.
+            distribution.
         mean: float
-        Specifies the mean of the normal distribution
+            Specifies the mean of the normal distribution
         """
 
         self.sigma = sigma
@@ -292,3 +295,78 @@ class LognormalPrior(BasePrior):
             The gradient of the prior at theta.
         """
         pass
+
+
+class NormalPrior(BasePrior):
+    def __init__(self, sigma, mean=0):
+        """
+        Normal prior
+
+        Parameters
+        ----------
+        sigma: float
+            Specifies the standard deviation of the normal
+            distribution.
+        mean: float
+            Specifies the mean of the normal distribution
+        """
+
+        self.sigma = sigma
+        self.mean = mean
+
+    def lnprob(self, theta):
+        """
+        Returns the pdf of theta. Note: theta should
+        be on a log scale.
+
+        Parameters
+        ----------
+        theta : (D,) numpy array
+            A hyperparameter configuration in log space.
+
+        Returns
+        -------
+        float
+            The log probability of theta
+        """
+
+        return sps.norm.pdf(theta, scale=self.sigma, loc=self.mean)
+
+    def sample_from_prior(self, n_samples):
+        """
+        Returns N samples from the prior.
+
+        Parameters
+        ----------
+        n_samples : int
+            The number of samples that will be drawn.
+
+        Returns
+        -------
+        (N, D) np.array
+            The samples from the prior.
+        """
+
+        p0 = np.random.normal(loc=self.mean,
+                              scale=self.sigma,
+                              size=n_samples)
+        return p0[:, np.newaxis]
+
+    def gradient(self, theta):
+        """
+        Computes the gradient of the prior with
+        respect to theta.
+
+        Parameters
+        ----------
+        theta : (D,) numpy array
+            Hyperparameter configuration in log space
+
+        Returns
+        -------
+        (D) np.array
+            The gradient of the prior at theta.
+        """
+        #return sps.norm.pdf(theta, scale=self.sigma, loc=self.mean)
+        return (1 / (self.sigma * np.sqrt(2 * np.pi))) * (- theta /
+            (self.sigma ** 2) * np.exp(- (theta ** 2) / (2 * self.sigma ** 2)))
--- robo/test/test_priors.py
+++ robo/test/test_priors.py
@@ -9,9 +9,13 @@ Created on Tue Dec  8 00:25:52 2015
 import unittest
 import numpy as np
 
-from robo.priors.default_priors import TophatPrior
-from robo.priors.default_priors import LognormalPrior
-from robo.priors.default_priors import HorseshoePrior
+from scipy.optimize import check_grad
+
+
+from robo.priors.base_prior import TophatPrior
+from robo.priors.base_prior import LognormalPrior
+from robo.priors.base_prior import HorseshoePrior
+from robo.priors.base_prior import NormalPrior
 
 
 class TestTophatPrior(unittest.TestCase):
@@ -20,7 +24,7 @@ class TestTophatPrior(unittest.TestCase):
         l = -2
         u = 2
         prior = TophatPrior(l, u)
-        
+
         # Check sampling
         p0 = prior.sample_from_prior(10)
         assert len(p0.shape) == 2
@@ -28,7 +32,7 @@ class TestTophatPrior(unittest.TestCase):
         assert p0.shape[1] == 1
 
         # Check gradients
-        
+
         # Check likelihood
         theta = np.array([0])
         assert prior.lnprob(theta) == 0
@@ -40,7 +44,7 @@ class TestHorseshoePrior(unittest.TestCase):
 
     def test(self):
         prior = HorseshoePrior()
-        
+
         # Check sampling
         p0 = prior.sample_from_prior(10)
         assert len(p0.shape) == 2
@@ -48,13 +52,15 @@ class TestHorseshoePrior(unittest.TestCase):
         assert p0.shape[1] == 1
 
         # Check gradients
- 
+        x_ = np.array([[0.1]])
+        assert check_grad(prior.lnprob, prior.gradient, x_) <= 1e-5
+
 
 class TestLognormalPrior(unittest.TestCase):
 
     def test(self):
         prior = LognormalPrior(sigma=0.1)
-        
+
         # Check sampling
         p0 = prior.sample_from_prior(10)
         assert len(p0.shape) == 2
@@ -62,7 +68,23 @@ class TestLognormalPrior(unittest.TestCase):
         assert p0.shape[1] == 1
 
         # Check gradients
-               
+
+
+class TestNormalPrior(unittest.TestCase):
+
+    def test(self):
+        prior = NormalPrior(mean=0, sigma=1)
+
+        # Check sampling
+        p0 = prior.sample_from_prior(10)
+        assert len(p0.shape) == 2
+        assert p0.shape[0] == 10
+        assert p0.shape[1] == 1
+
+        # Check gradients
+        x_ = np.array([[0.1]])
+        assert check_grad(prior.lnprob, prior.gradient, x_) <= 1e-5
+
 
 if __name__ == "__main__":
     unittest.main()
