86f4950f0de39ca8e7310b121a40a0e0e5077bd5
Aaron Klein <kleinaa@cs.uni-freiburg.de>
2016-02-11 17:09:12 +0100

    pep8

--- robo/test/test_random_forest.py
+++ robo/test/test_random_forest.py
@@ -14,7 +14,9 @@ from robo.acquisition.ei import EI
 from robo.acquisition.pi import PI
 from robo.acquisition.lcb import LCB
 from robo.incumbent.best_observation import BestObservation
-from robo.incumbent.posterior_optimization import PosteriorMeanOptimization, PosteriorMeanAndStdOptimization
+from robo.incumbent.posterior_optimization import PosteriorMeanOptimization
+from robo.incumbent.posterior_optimization import PosteriorMeanAndStdOptimization
+
 
 class TestRandomForest(unittest.TestCase):
 
@@ -26,7 +28,7 @@ class TestRandomForest(unittest.TestCase):
 
         model = RandomForest(types=np.zeros([X_lower.shape[0]]))
         model.train(X, Y)
-        
+
         x_test = init_random_uniform(X_lower, X_upper, 3)
 
         # Shape matching predict
@@ -42,11 +44,11 @@ class TestRandomForest(unittest.TestCase):
         # Shape matching function sampling
         x_ = np.linspace(X_lower, X_upper, 10)
         x_ = x_[:, np.newaxis]
-        #funcs = model.sample_functions(x_, n_funcs=2)        
+        #funcs = model.sample_functions(x_, n_funcs=2)
         #assert len(funcs.shape) == 2
         #assert funcs.shape[0] == 2
-        #assert funcs.shape[1] == x_.shape[0]        
-        
+        #assert funcs.shape[1] == x_.shape[0]
+
         # Check compatibility with all acquisition functions
         acq_func = EI(model,
                      X_upper=X_upper,
@@ -66,38 +68,35 @@ class TestRandomForest(unittest.TestCase):
         acq_func.update(model)
         acq_func(x_test)
 
-	# Check compatibility with all incumbent estimation methods
-	rec = BestObservation(model, X_lower, X_upper)
-	inc, inc_val = rec.estimate_incumbent(None)
-	assert len(inc.shape) == 2
-	assert inc.shape[0] == 1
-	assert inc.shape[1] == X_upper.shape[0]
-	assert len(inc_val.shape) == 2
-	assert inc_val.shape[0] == 1
-	assert inc_val.shape[1] == 1
+        # Check compatibility with all incumbent estimation methods
+        rec = BestObservation(model, X_lower, X_upper)
+        inc, inc_val = rec.estimate_incumbent(None)
+        assert len(inc.shape) == 2
+        assert inc.shape[0] == 1
+        assert inc.shape[1] == X_upper.shape[0]
+        assert len(inc_val.shape) == 2
+        assert inc_val.shape[0] == 1
+        assert inc_val.shape[1] == 1
 
-	rec = PosteriorMeanOptimization(model, X_lower, X_upper)
-	startpoints = init_random_uniform(X_lower, X_upper, 4)
-	inc, inc_val = rec.estimate_incumbent(startpoints)
-	assert len(inc.shape) == 2
-	assert inc.shape[0] == 1
-	assert inc.shape[1] == X_upper.shape[0]
-	assert len(inc_val.shape) == 2
-	assert inc_val.shape[0] == 1
-	assert inc_val.shape[1] == 1
+        rec = PosteriorMeanOptimization(model, X_lower, X_upper)
+        startpoints = init_random_uniform(X_lower, X_upper, 4)
+        inc, inc_val = rec.estimate_incumbent(startpoints)
+        assert len(inc.shape) == 2
+        assert inc.shape[0] == 1
+        assert inc.shape[1] == X_upper.shape[0]
+        assert len(inc_val.shape) == 2
+        assert inc_val.shape[0] == 1
+        assert inc_val.shape[1] == 1
 
-	rec = PosteriorMeanAndStdOptimization(model, X_lower, X_upper)
-	startpoints = init_random_uniform(X_lower, X_upper, 4)
-	inc, inc_val = rec.estimate_incumbent(startpoints)
-	assert len(inc.shape) == 2
-	assert inc.shape[0] == 1
-	assert inc.shape[1] == X_upper.shape[0]
-	assert len(inc_val.shape) == 2
-	assert inc_val.shape[0] == 1
-	assert inc_val.shape[1] == 1
-
-
-
+        rec = PosteriorMeanAndStdOptimization(model, X_lower, X_upper)
+        startpoints = init_random_uniform(X_lower, X_upper, 4)
+        inc, inc_val = rec.estimate_incumbent(startpoints)
+        assert len(inc.shape) == 2
+        assert inc.shape[0] == 1
+        assert inc.shape[1] == X_upper.shape[0]
+        assert len(inc_val.shape) == 2
+        assert inc_val.shape[0] == 1
+        assert inc_val.shape[1] == 1
 
 if __name__ == "__main__":
     unittest.main()
