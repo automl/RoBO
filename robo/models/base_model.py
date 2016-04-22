986c062890bc6d14ed04ee4604db781773821b47
Aaron Klein <kleinaa@cs.uni-freiburg.de>
2016-04-22 10:20:51 +0200

    changed docstings

--- robo/models/base_model.py
+++ robo/models/base_model.py
@@ -13,13 +13,17 @@ class BaseModel(object):
 
     def train(self, X, y):
         """
-            Trains the model on the provided data.
-            :param X: Input datapoints. The dimensionality of X is (N, D),
-                    with N as the number of points and D is the number of features.
-            :type X: np.ndarray (N, D)
-            :param Y: The corresponding target values.
-                    The dimensionality of Y is (N), where N has to match the number of points of X
-            :type Y: np.ndarray (N)
+        Trains the model on the provided data.
+            
+        Parameters
+        ----------
+        X: np.ndarray (N, D)
+            Input datapoints. The dimensionality of X is (N, D),
+            with N as the number of points and D is the number of features.
+        Y: np.ndarray (N, T)
+            The corresponding target values.
+            The dimensionality of Y is (N, T), where N has to 
+            match the number of points of X and T is the number of objectives
         """
         self.X = X
         self.y = y
@@ -31,22 +35,35 @@ class BaseModel(object):
 
     def predict(self, X):
         """
-            Predicts for a given X matrix the target values
-            :param X: Test datapoints. The dimensionality of X is (N, D),
-                    with N as the number of points and D is the number of features.
-            :type X: np.ndarray (N, D)
-            :return The mean and variance of the testdatapoints.
+        Predicts for a given X matrix the target values
+        
+        Parameters
+        ----------
+        X: np.ndarray (N, D)
+            Test datapoints. The dimensionality of X is (N, D),
+            with N as the number of points and D is the number of features.
+            
+        Returns
+        ----------
+            The mean and variance of the test datapoint.
         """
         raise NotImplementedError()
 
     def predict_variance(self, X1, X2):
         raise NotImplementedError()
 
-    def predictive_gradients(self, Xnew, X=None):
+    def predictive_gradients(self, X=None):
         """
         Calculates the predictive gradients (gradient of the prediction)
-        :param Xnew: The points to predict the gradient for
-        :param X: TODO: Not implemented yet
-        :return: Gradients(?)
+        
+        Parameters
+        ----------
+        
+        X: np.ndarray (N, D)
+            The points to predict the gradient for
+
+        Returns
+        ----------
+            The gradients at X
         """
         raise NotImplementedError()
