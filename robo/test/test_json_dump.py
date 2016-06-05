18e14ba67c245f2a9ec24b05d5c504452a1f299d
numairmansur <numair.mansur@gmail.com>
2016-06-05 22:35:44 +0200

    Create Json unit test file

---
+++ robo/test/test_json_dump.py
@@ -0,0 +1,22 @@
+'''
+
+Created on: June 5th, 2016
+@author: Numair Mansur (numair.mansur@gmail.com)
+
+'''
+
+
+import unittest
+class TestJsonMethods(unittest.TestCase):
+
+    def test_json_base_solver(self):
+        assert True
+
+    def test_json_base_model(self):
+        assert True
+
+    def test_json_base_task(self):
+        assert True
+
+    def test_json_base_acquisition(self):
+        assert True
