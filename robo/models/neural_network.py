26955290902c3f8dcc6d9709a83e7525c8abcc55
Stefan Falkner <sfalkner@cs.uni-freiburg.de>
2016-10-17 15:23:29 +0200

    addad a flexible NN model to be used with bagging and retraining

---
+++ robo/models/neural_network.py
@@ -0,0 +1,275 @@
+import time
+import lasagne
+import logging
+import theano
+import theano.tensor as T
+import numpy as np
+
+
+class SGDNet(object):
+
+    def __init__(self, n_inputs,  n_epochs=100, l_rate=1e-3,
+                 n_units= [10, 10, 10],
+                 dropout = [0.5,0.5,0.5],
+                 batch_size=10, shuffle_batches = False,
+                 normalize_output=True,
+                 normalize_input=True, rng=None):
+        """
+        Constructor
+
+        Parameters
+        ----------
+        n_inputs : int
+            Number of input features
+        """
+
+        if rng is None:
+            self.rng = np.random.RandomState()
+        else:
+            self.rng = rng
+        lasagne.random.set_rng(self.rng)
+
+        self.n_inputs = n_inputs
+        self.n_epochs = n_epochs
+        self.l_rate = l_rate
+        self.n_units = n_units
+
+
+        # bring the dropout parameters in a common form
+        try:
+            if len(dropout) != len(n_units):
+                raise ValueError("Number of dropout rates must match the number of layers")
+            self.dropout = dropout
+        except TypeError as e:
+            self.dropout = [dropout] * len(n_units)
+        except:
+            raise
+        
+        self.batch_size = batch_size
+        self.shuffle_batches = shuffle_batches
+        self.is_trained = False
+        self.normalize_output = normalize_output
+        self.normalize_input = normalize_input
+
+        self.x_mean = None
+        self.x_std = None
+        self.y_mean = None
+        self.y_std = None
+        self.input_var = T.matrix('inputs')
+        target_var = T.matrix('targets')
+
+        self.initialize_weights()
+
+        prediction = lasagne.layers.get_output(self.network)
+        loss = lasagne.objectives.squared_error(prediction, target_var)
+        loss = loss.mean()
+
+        params = lasagne.layers.get_all_params(self.network, trainable=True)
+        updates = lasagne.updates.adam(loss, params, learning_rate=l_rate)
+
+        self.train_fn = theano.function([self.input_var, target_var], loss, updates=updates)
+
+        test_prediction = lasagne.layers.get_output(self.network, deterministic=True)
+        test_loss = lasagne.objectives.squared_error(test_prediction, target_var)
+        test_loss = test_loss.mean()
+
+        self.val_fn = theano.function([self.input_var, target_var], test_loss)
+
+    def initialize_weights(self):
+        self.network = lasagne.layers.InputLayer(shape=(None, self.n_inputs), input_var=self.input_var)
+
+        for n,p in zip(self.n_units, self.dropout):
+            self.network = lasagne.layers.DenseLayer(
+                self.network,
+                num_units=n,
+                W=lasagne.init.HeNormal(),
+                b=lasagne.init.Constant(val=0.0),
+                nonlinearity=lasagne.nonlinearities.tanh)
+            self.network = lasagne.layers.dropout(self.network, p=p)
+
+        self.network = lasagne.layers.DenseLayer(
+            self.network,
+            num_units=1,
+            W=lasagne.init.HeNormal(),
+            b=lasagne.init.Constant(val=0.0),
+            nonlinearity=lasagne.nonlinearities.linear)
+
+
+    def train(self, X, Y, X_valid, Y_valid):
+        """
+        Trains the model on the provided data.
+
+        Parameters
+        ----------
+        X: np.ndarray (N, D)
+            Input data points. The dimensionality of X is (N, D),
+            with N as the number of points and D is the number of features.
+        Y: np.ndarray (N, T)
+            The corresponding target values.
+            The dimensionality of Y is (N, T), where N has to
+            match the number of points of X and T is the number of objectives
+        """
+
+        if self.normalize_input:
+            X, self.x_mean, self.x_std = self.normalize_inputs(X)
+            X_valid = self.normalize_inputs(X_valid, self.x_mean, self.x_std)[0]
+        else:
+            self.x_mean, self.x_std = None, None
+
+        if self.normalize_output:
+            Y, self.y_mean, self.y_std = self.normalize_targets(Y)
+            Y_valid = self.normalize_targets(Y_valid, self.y_mean, self.y_std)[0]
+            
+        else:
+            self.Y, self.y_mean, self.y_std = Y, None, None
+
+        self.initialize_weights()
+        logging.info("Starting training...")
+
+        if X.shape[0] < self.batch_size:
+            batch_size = X.shape[0]
+        else:
+            batch_size = self.batch_size
+
+        for epoch in range(self.n_epochs):
+
+            train_err = 0
+            train_batches = 0
+            start_time = time.time()
+            for batch in self.iterate_minibatches(X, Y, batch_size, shuffle=self.shuffle_batches):
+                inputs, targets = batch
+                train_err += self.train_fn(inputs, targets[:,None])
+                train_batches += 1
+
+            logging.info("Epoch {} of {} took {:.3f}s".format(epoch + 1, self.n_epochs, time.time() - start_time))
+            logging.info("  training loss:\t\t{:.6f}".format(train_err / train_batches))
+
+        val_err = 0
+
+        val_batches = 0
+        for batch in self.iterate_minibatches(X_valid, Y_valid, batch_size, shuffle=False):
+            inputs, targets = batch
+            err = self.val_fn(inputs, targets[:,None])
+            val_err += err
+            val_batches += 1
+        logging.info("  valid loss:\t\t{:.6f}".format(val_err / val_batches))
+
+        self.is_trained = True
+
+        return val_err / val_batches
+
+    def predict(self, X_test):
+        """
+        makes predictions for all given points
+
+        Parameters
+        ----------
+        X_test : numpy.array
+            2d matrix with points as rows
+
+        Returns
+        ----------
+        float
+            predictions for all points
+        """
+
+        if not self.is_trained:
+            logging.error("Model is not trained!")
+            return
+
+        if self.normalize_input:
+            X_test = self.normalize_inputs(X_test, self.x_mean, self.x_std)[0]
+
+        l = lasagne.layers.get_all_layers(self.network)
+        m = lasagne.layers.get_output(l, X_test)[-1].eval()
+
+        # denormalize output
+        if self.normalize_output:
+            m = self.denormalize(m, self.y_mean, self.y_std)
+
+        return m[:, None]
+
+    def iterate_minibatches(self, inputs, targets, batchsize, shuffle=False):
+        assert len(inputs) == len(targets)
+        if shuffle:
+            indices = np.arange(len(inputs))
+            np.random.shuffle(indices)
+        for start_idx in range(0, len(inputs) - batchsize + 1, batchsize):
+            if shuffle:
+                excerpt = indices[start_idx:start_idx + batchsize]
+            else:
+                excerpt = slice(start_idx, start_idx + batchsize)
+            yield inputs[excerpt], targets[excerpt]
+
+
+    @staticmethod
+    def normalize_inputs(x, mean=None, std=None):
+        """
+        normalize_inputs
+
+        Parameters
+        ----------
+        x : numpy.array
+            input vector
+        mean: float
+            mean used for normalization
+        std: float
+            standard deviation used for normalization
+
+        Returns
+        ----------
+        float
+            scaled x as input to the network
+        """
+        if mean is None:
+            mean = np.mean(x, axis=0)
+        if std is None:
+            std = np.std(x, axis=0)
+        return (x - mean) / std, mean, std
+
+    @staticmethod
+    def normalize_targets(y, mean=None, std=None):
+        """
+        normalize_targets
+
+        Parameters
+        ----------
+        y : numpy.array
+            target values
+        mean: float
+            mean used for normalization
+        std: float
+            standard deviation used for normalization
+
+        Returns
+        ----------
+        float
+            scaled y as target output for the network
+        """
+        if mean is None:
+            mean = np.mean(y, axis=0)
+        if std is None:
+            std = np.std(y, axis=0)
+        return (y - mean) / std, mean, std
+
+    @staticmethod
+    def denormalize(x, mean, std):
+        """
+        denormalize
+
+        Parameters
+        ----------
+        y : numpy.array
+            target values
+        mean: float
+            mean used for normalization
+        std: float
+            standard deviation used for normalization
+
+        Returns
+        ----------
+        float
+            scaled y as target output for the network
+
+        """
+        return mean + x * std
