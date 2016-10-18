82a8d5f890ce72dac34cd851cc7ca590c09c1bfd
Aaron Klein <kleinaa@cs.uni-freiburg.de>
2016-10-18 16:43:07 +0200

    minor changes

--- examples/example_bnn.py
+++ examples/example_bnn.py
@@ -2,8 +2,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 import logging
 
-from robo.models.bnn import SGLDNet
-from robo.initial_design.init_random_uniform import  init_random_uniform
+from robo.models.bnn import BayesianNeuralNetwork
+from robo.initial_design.init_random_uniform import init_random_uniform
 
 
 def f(x):
@@ -16,10 +16,25 @@ rng = np.random.RandomState(42)
 X = init_random_uniform(np.zeros(1), np.ones(1), 50, rng)
 Y = f(X)
 
-model = SGLDNet(l_rate=1e-3, burn_in=30000, n_iters=100000)
-model.train(X, Y)
+#model = BayesianNeuralNetwork(sampling_method="sgld", l_rate=1e-3, burn_in=30000, n_iters=100000)
+#model.train(X, Y)
 
 x = np.linspace(0, 1, 100)[:, None]
+
+vals = f(x)
+
+#mean_pred, var_pred = model.predict(x)
+
+#std_pred = np.sqrt(var_pred)
+
+plt.grid()
+#plt.plot(x[:, 0], mean_pred[:, 0], label="SGLD", color="red")
+plt.plot(x[:, 0], vals[:, 0], label="true", color="green")
+plt.plot(X[:, 0], Y[:, 0], "ro")
+#plt.fill_between(x[:, 0], mean_pred[:, 0] + std_pred[:, 0], mean_pred[:, 0] - std_pred[:, 0], alpha=0.2, color="orange")
+
+model = BayesianNeuralNetwork(sampling_method="sghmc", l_rate=1e-4, burn_in=20000, n_iters=50000)
+model.train(X, Y)
 
 vals = f(x)
 
@@ -27,11 +42,8 @@ mean_pred, var_pred = model.predict(x)
 
 std_pred = np.sqrt(var_pred)
 
-plt.grid()
-plt.plot(x[:, 0], mean_pred[:, 0], label="mean", color="blue")
-plt.plot(x[:, 0], vals[:, 0], label="true", color="green")
-plt.plot(X[:, 0], Y[:, 0], "ro")
-plt.fill_between(x[:, 0], mean_pred[:, 0] + std_pred[:, 0], mean_pred[:, 0] - std_pred[:, 0], alpha=0.2, color="orange")
+plt.plot(x[:, 0], mean_pred[:, 0], label="SGHMC", color="blue")
+plt.fill_between(x[:, 0], mean_pred[:, 0] + std_pred[:, 0], mean_pred[:, 0] - std_pred[:, 0], alpha=0.2, color="blue")
 
 plt.legend()
 plt.show()
