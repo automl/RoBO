adf154613802dc25737bd6d11e8e024e2d83ece5
Aaron Klein <kleinaa@cs.uni-freiburg.de>
2016-10-26 11:27:31 +0200

    itendation and other fixes

--- examples/example_bagged_nets.py
+++ examples/example_bagged_nets.py
@@ -4,14 +4,9 @@ import logging
 import numpy as np
 import matplotlib.pyplot as plt
 
-
-import sklearn.datasets as data
-from sklearn.model_selection import train_test_split
 import robo.models.neural_network as robo_net
 import robo.models.bagged_networks as bn
 from robo.initial_design.init_random_uniform import init_random_uniform
-
-from IPython import embed
 
 logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 
@@ -30,21 +25,17 @@ x = np.linspace(0, 1, 512, dtype=np.float32)[:, None]
 vals = f(x).astype(np.float32)
 
 plt.grid()
-#plt.plot(x[:, 0], mean_pred[:, 0], label="SGLD", color="red")
 plt.plot(x[:, 0], f(x)[:, 0], label="true", color="green")
 plt.plot(X[:, 0], Y[:, 0], "ro")
 
 
-
-model = bn.BaggedNets(robo_net.SGDNet, num_models = 16, bootstrap_with_replacement=False,
-					# the remaining arguments are passed to the init of the base model
-					n_epochs = 16384, error_threshold=1e-3, 
-					n_units=[32, 32, 32], dropout = 0,
-					batch_size=10, learning_rate=1e-3,
-					shuffle_batches=True)
+model = bn.BaggedNets(robo_net.SGDNet, num_models=16, bootstrap_with_replacement=True,
+                      n_epochs=16384, error_threshold=1e-3,
+                      n_units=[32, 32, 32], dropout=0,
+                      batch_size=10, learning_rate=1e-3,
+                      shuffle_batches=True)
 
 m = model.train(X, Y)
-
 
 mean_pred, var_pred = model.predict(x)
 std_pred = np.sqrt(var_pred)
@@ -52,22 +43,5 @@ std_pred = np.sqrt(var_pred)
 plt.plot(x[:, 0], mean_pred[:, 0], label="bagged nets", color="blue")
 plt.fill_between(x[:, 0], mean_pred[:, 0] + std_pred[:, 0], mean_pred[:, 0] - std_pred[:, 0], alpha=0.2, color="blue")
 
-
-
-
-"""
-for i in range(len(model.models)):
-	plt.figure()
-	plt.scatter(X[model.bootstrap_indices[i],0], Y[model.bootstrap_indices[i],0])
-	plt.plot(x, model.models[i].predict(x)[:,0])
-"""
-
-
-
-
-
 plt.legend()
 plt.show()
-
-
-embed()
