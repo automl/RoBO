998de658a973813772e9243d9d3a55e02edbbe55
Aaron Klein <kleinaa@cs.uni-freiburg.de>
2016-11-11 15:17:04 +0100

    refactor initial design

--- robo/initial_design/init_random_normal.py
+++ robo/initial_design/init_random_normal.py
@@ -2,36 +2,41 @@
 import numpy as np
 
 
-def init_random_normal(X_lower, X_upper, N, mean=None, std=None, rng=None):
+def init_random_normal(lower, upper, n_points, mean=None, std=None, rng=None):
     """
     Returns as initial design N data points sampled from a normal
     distribution.
 
     Parameters
     ----------
-    X_lower: np.ndarray (D)
-        Lower bounds of the input space
-    X_upper: np.ndarray (D)
-        Upper bounds of the input space
-    N: int
+    lower: np.ndarray (D)
+        Lower bound of the input space
+    upper: np.ndarray (D)
+        Upper bound of the input space
+    n_points: int
         The number of initial data points
-    mean: float
-        Mean of the normal distribution
-    std: float
-        Std of the normal distribution
-    rng: numpy.random.RandomState
+    mean: np.ndarray (D)
+        Mean of the normal distribution for each dimension
+    std: np.ndarray (D)
+        Std of the normal distribution for each dimension
+    rng: np.random.RandomState
+            Random number generator
 
     Returns
     -------
-    np.ndarray(N,D)
+    np.ndarray(N, D)
         The initial design data points
     """
     if rng is None:
         rng = np.random.RandomState(np.random.randint(0, 10000))
+
+    n_dims = lower.shape[0]
+    # take the center as mean
     if mean is None:
-        mean = (X_upper + X_lower) * 0.5
+        mean = (upper + lower) * 0.5
+
     if std is None:
-        std = (X_lower - X_upper) * 0.5
-    n_dims = X_lower.shape[0]
-    return np.array([np.clip(rng.normal(mean[i], std[i], N), X_lower[i], X_upper[i])
+        std = np.ones([n_dims]) * 0.1
+
+    return np.array([np.clip(rng.normal(mean[i], std[i], n_points), lower[i], upper[i])
                      for i in range(n_dims)]).T
--- robo/initial_design/init_random_uniform.py
+++ robo/initial_design/init_random_uniform.py
@@ -2,25 +2,29 @@
 import numpy as np
 
 
-def init_random_uniform(X_lower, X_upper, N, rng=None):
+def init_random_uniform(lower, upper, n_points, rng=None):
     """
     Samples N data points uniformly.
 
     Parameters
     ----------
-    X_lower: np.ndarray (D)
+    lower: np.ndarray (D)
         Lower bounds of the input space
-    X_upper: np.ndarray (D)
+    upper: np.ndarray (D)
         Upper bounds of the input space
-    N: int
+    n_points: int
         The number of initial data points
-
+    rng: np.random.RandomState
+            Random number generator
     Returns
     -------
     np.ndarray(N,D)
         The initial design data points
     """
+
     if rng is None:
         rng = np.random.RandomState(np.random.randint(0, 10000))
-    n_dims = X_lower.shape[0]
-    return np.array([rng.uniform(X_lower, X_upper, n_dims) for _ in range(N)])
+
+    n_dims = lower.shape[0]
+
+    return np.array([rng.uniform(lower, upper, n_dims) for _ in range(n_points)])
