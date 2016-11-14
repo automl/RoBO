eeeb93e13d04654cf71d44bf444f7bc9c1148fbc
Aaron Klein <kleinaa@cs.uni-freiburg.de>
2016-11-14 15:53:47 +0100

    option to make direct quiet

--- robo/maximizers/direct.py
+++ robo/maximizers/direct.py
@@ -1,6 +1,6 @@
-
+import sys
+import os
 import DIRECT
-
 import numpy as np
 
 from robo.maximizers.base_maximizer import BaseMaximizer
@@ -9,7 +9,7 @@ from robo.maximizers.base_maximizer import BaseMaximizer
 class Direct(BaseMaximizer):
 
     def __init__(self, objective_function, lower, upper,
-                 n_func_evals=400, n_iters=200):
+                 n_func_evals=400, n_iters=200, verbose=False):
         """
         Interface for the DIRECT algorithm by D. R. Jones, C. D. Perttunen
         and B. E. Stuckmann
@@ -26,9 +26,13 @@ class Direct(BaseMaximizer):
             The maximum number of function evaluations
         n_iters: int
             The maximum number of iterations
+        verbose: bool
+            Suppress Direct's output.
         """
         self.n_func_evals = n_func_evals
         self.n_iters = n_iters
+        self.verbose = verbose
+
         super(Direct, self).__init__(objective_function, lower, upper)
 
     def _direct_acquisition_fkt_wrapper(self, acq_f):
@@ -44,10 +48,25 @@ class Direct(BaseMaximizer):
         -------
         np.ndarray(N,D)
             Point with highest acquisition value.
+
         """
-        x, _, _ = DIRECT.solve(self._direct_acquisition_fkt_wrapper(self.objective_func),
+        if self.verbose:
+            x, _, _ = DIRECT.solve(self._direct_acquisition_fkt_wrapper(self.objective_func),
                                l=[self.lower],
                                u=[self.upper],
                                maxT=self.n_iters,
                                maxf=self.n_func_evals)
+        else:
+            fileno = sys.stdout.fileno()
+            with os.fdopen(os.dup(fileno), 'wb') as stdout:
+                with os.fdopen(os.open(os.devnull, os.O_WRONLY), 'wb') as devnull:
+                    sys.stdout.flush();
+                    os.dup2(devnull.fileno(), fileno)  # redirect
+                    x, _, _ = DIRECT.solve(self._direct_acquisition_fkt_wrapper(self.objective_func),
+                                           l=[self.lower],
+                                           u=[self.upper],
+                                           maxT=self.n_iters,
+                                           maxf=self.n_func_evals)
+                sys.stdout.flush();
+                os.dup2(stdout.fileno(), fileno)  # restore
         return x
