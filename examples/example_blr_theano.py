4a652b9131ae62008690bf8e0bd71989b9321449
Stefan Falkner <stefan_falkner@freenet.de>
2016-11-14 23:13:53 +0100

    blr with theano, computation of the marginal likelihood works, but no HMC sampling yet

--- examples/example_blr_theano.py
+++ examples/example_blr_theano.py
@@ -1,22 +1,88 @@
-import numpy as np
+import logging
+
 
 import theano
 import theano.tensor as T
-import theano.tensor.nlinalg
+import theano.tensor.nlinalg as nlinalg
 
+import matplotlib.pyplot as plt
+
+
+import numpy as np
 from scipy import optimize
 
+logger = logging.getLogger(__name__)
 
+
+
+def quadratic_basis_func(x):
+	x = np.append(x**2, x, axis=1)
+	return np.append(x, np.ones([x.shape[0], 1]), axis=1)
+
+
+
+def linear_basis_func(x):
+	return np.append(x, np.ones([x.shape[0], 1]), axis=1)
+
+
+
+
+def static_marginal_log_likelihood(Phi, y, theta):
+	"""
+	Log likelihood of the data marginalised over the weights w. See chapter 3.5 of
+	the book by Bishop of an derivation.
+
+	Parameters
+	----------
+	theta: np.array(2,)
+		The hyperparameter alpha and beta on a log scale
+
+	Returns
+	-------
+	float
+		lnlikelihood + prior
+	"""
+
+	# Theta is on a log scale
+	alpha = np.exp(theta[0])
+	beta = np.exp(theta[1])
+
+	D = Phi.shape[1]
+	N = Phi.shape[0]
+
+	A = beta * T.dot(Phi.T, Phi)
+	A += T.eye(T.shape(Phi)[1]) * alpha
+	A_inv = nlinalg.matrix_inverse(A)
+	m = beta * T.dot(A_inv, Phi.T)
+	m = T.dot(m, y)
+
+	mll = D / 2 * T.log(alpha)
+	mll += N / 2 * T.log(beta)
+	mll -= N / 2 * T.log(2 * np.pi)
+	mll -= beta / 2. * T.sum(T.power(y - T.dot(Phi, m), 2))
+	mll -= alpha / 2. * T.dot(m.T, m)
+	mll -= 0.5 * T.log(nlinalg.det(A))
+
+	return mll
+
+
+
+Phi = T.dmatrix('Phi')
+y = T.dvector('y')
+theta = T.dvector('theta')
+
+marginal_log_likelihood_theano = theano.function([Phi, y, theta], static_marginal_log_likelihood(Phi,y,theta))
 
 
 class BayesianLinearRegression(object):
 
-	def __init__(self, alpha=1, beta=1000, basis_func=None,
+	def __init__(self, alpha=1, beta=1000, basis_func=linear_basis_func,
 				 prior=None, do_mcmc=True, n_hypers=20, chain_length=2000,
 				 burnin_steps=2000, rng=None):
 		"""
 		Implementation of Bayesian linear regression. See chapter 3.3 of the book
 		"Pattern Recognition and Machine Learning" by Bishop for more details.
+
 		Parameters
 		----------
 		alpha: float
@@ -46,7 +112,8 @@ class BayesianLinearRegression(object):
 		else:
 			self.rng = rng
 
-
+		self.X = None
+		self.y = None
 		self.alpha = alpha
 		self.beta = beta
 		self.basis_func = basis_func
@@ -58,78 +125,17 @@ class BayesianLinearRegression(object):
 		self.burnin_steps = burnin_steps
 		self.models = None
 
-		theta = T.dvector('theta')
-		self.Phi = theano.shared('Phi')
-		self.y = T.dmatrix('y')
-		
-		self.mll = theano.function([theta], self.marginal_log_likelihood_theano(theta),on_unused_input='warn')
-		
+
 
 	def marginal_log_likelihood(self, theta):
-		"""
-		Log likelihood of the data marginalised over the weights w. See chapter 3.5 of
-		the book by Bishop of an derivation.
-		Parameters
-		----------
-		theta: np.array(2,)
-			The hyperparameter alpha and beta on a log scale
-		Returns
-		-------
-		float
-			lnlikelihood + prior
-		"""
-		return(self.mll(theta))
-
-
-	def marginal_log_likelihood_theano(self, theta):
-		"""
-		Log likelihood of the data marginalised over the weights w. See chapter 3.5 of
-		the book by Bishop of an derivation.
-		Parameters
-		----------
-		theta: np.array(2,)
-			The hyperparameter alpha and beta on a log scale
-		Returns
-		-------
-		float
-			lnlikelihood + prior
-		"""
-		
-		# Theta is on a logscale        
-		t = T.exp(theta)
-		alpha = t[0]
-		beta = t[1]
-
-		N = T.shape(X)[0]	# number of data points
-		M = T.shape(X)[1]	# number of features
-
-		A =  beta  * T.dot(self.Phi.T, X)
-		A += alpha * T.identity_like(A)
-
-		A_inv = T.inv(A)
-
-		m = beta * T.dot(T.dot(A, self.Phi.T), y)
-
-		mll = M / 2 * T.log(alpha)
-		mll += N / 2 * np.log(beta)
-		
-		mll -= beta / 2. * (y - T.dot(X, m)).norm(2)
-		mll -= alpha / 2. * T.dot(m.T, m)
-
-		mll -= 0.5 * T.log(T.nlinalg.det(A))
-
-
-		if self.prior is not None:
-			mll += self.prior.lnprob(theta)
-
-		return(mll)
-
+		return(marginal_log_likelihood_theano(self.X_transformed, self.y, theta))
 
 	def train(self, X, y, do_optimize=True):
 		"""
 		First optimized the hyperparameters if do_optimize is True and then computes
 		the posterior distribution of the weights. See chapter 3.3 of the book by Bishop
 		for more details.
+
 		Parameters
 		----------
 		X: np.ndarray (N, D)
@@ -145,18 +151,43 @@ class BayesianLinearRegression(object):
 		self.X = X
 
 		if self.basis_func is not None:
-			self.Phi = self.basis_func(X)
+			self.X_transformed = self.basis_func(X)
 		else:
-			self.Phi = self.X
+			self.X_transformed = self.X
 
 		self.y = y
 
 		if do_optimize:
 			if self.do_mcmc:
-				raise NotImplementedError("TODO")
+				sampler = emcee.EnsembleSampler(self.n_hypers, 2,
+												self.marginal_log_likelihood)
+
+				# Do a burn-in in the first iteration
+				if not self.burned:
+					# Initialize the walkers by sampling from the prior
+					self.p0 = self.prior.sample_from_prior(self.n_hypers)
+
+					# Run MCMC sampling
+					self.p0, _, _ = sampler.run_mcmc(self.p0,
+													 self.burnin_steps,
+													 rstate0=self.rng)
+	
+					self.burned = True
+	
+				# Start sampling
+				pos, _, _ = sampler.run_mcmc(self.p0,
+											 self.chain_length,
+											 rstate0=self.rng)
+	
+				# Save the current position, it will be the start point in
+				# the next iteration
+				self.p0 = pos
+	
+				# Take the last samples from each walker
+				self.hypers = np.exp(sampler.chain[:, -1])
 			else:
 				# Optimize hyperparameters of the Bayesian linear regression        
-				res = optimize.fmin(self.negative_mll, self.rng.rand(2))
+				res = optimize.fmin(lambda t: -self.marginal_log_likelihood(t),self.rng.rand(2))
 				self.hypers = [[np.exp(res[0]), np.exp(res[1])]]
 				
 		else:
@@ -169,29 +200,84 @@ class BayesianLinearRegression(object):
 
 			logger.debug("Alpha=%f ; Beta=%f" % (alpha, beta))
 
-			S_inv  = beta * np.dot(self.Phi.T, self.Phi)
-			S_inv += alpha* np.eye(self.Phi.shape[1])
+			S_inv = beta * np.dot(self.X_transformed.T, self.X_transformed)
+			S_inv += np.eye(self.X_transformed.shape[1]) * alpha
 
 			S = np.linalg.inv(S_inv)
-			m = beta * np.dot(np.dot(S, self.Phi.T), self.y)
+			m = beta * np.dot(np.dot(S, self.X_transformed.T), self.y)
 
 			self.models.append((m, S))
+
+	def predict(self, X_test):
+		r"""
+		Returns the predictive mean and variance of the objective function at
+		the given test points.
+
+		Parameters
+		----------
+		X_test: np.ndarray (N, D)
+			N input test points
+
+		Returns
+		----------
+		np.array(N,)
+			predictive mean
+		np.array(N,)
+			predictive variance
+
+		"""
+		if self.basis_func is not None:
+			X_transformed = self.basis_func(X_test)
+		else:
+			X_transformed = X_test
+			
+		# Marginalise predictions over hyperparameters
+		mu = np.zeros([len(self.hypers), X_transformed.shape[0]])
+		var = np.zeros([len(self.hypers), X_transformed.shape[0]])
+	
+		for i, h in enumerate(self.hypers):
+			mu[i] = np.dot(self.models[i][0].T, X_transformed.T)
+			var[i] = 1. / h[1] + np.diag(np.dot(np.dot(X_transformed, self.models[i][1]), X_transformed.T))
+
+		m = mu.mean(axis=0)
+		v = var.mean(axis=0)
+		# Clip negative variances and set them to the smallest
+		# positive float value
+		if v.shape[0] == 1:
+			v = np.clip(v, np.finfo(v.dtype).eps, np.inf)
+		else:
+			v = np.clip(v, np.finfo(v.dtype).eps, np.inf)
+			v[np.where((v < np.finfo(v.dtype).eps) & (v > -np.finfo(v.dtype).eps))] = 0
+
+		return m, v
+
 
 
 N = 20
 x = np.linspace(0,1,N)
 
-def quadratic_basis_func(x):
-	x = np.append(x**2, x, axis=1)
-	return np.append(x, np.ones([x.shape[0], 1]), axis=1)
 
+Phi = quadratic_basis_func(x[:,None])
 
-X = quadratic_basis_func(x[:,None])
-y = 0.5*x + 0.1
+#Phi = linear_basis_func(x[:,None])
+
+y = 0.5*x + 0 + np.random.randn(len(x))*0.02
 
 
 
 
 from IPython import embed
+
+model = BayesianLinearRegression(1000,1,do_mcmc=False)
+model.train(Phi,y,True)
+
+mu,var = model.predict(Phi)
+
+print(mu,var)
+
+plt.scatter(x,y)
+plt.plot(x,mu)
+plt.fill_between(x, mu-np.sqrt(var), mu+np.sqrt(var),alpha=0.5)
+plt.show()
 
 embed()
