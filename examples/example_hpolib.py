5a1645226a8f255b35fbeed4ed00397efa423c18
Aaron Klein <kleinaa@cs.uni-freiburg.de>
2016-11-16 09:25:06 +0100

    small fix

--- examples/example_hpolib.py
+++ examples/example_hpolib.py
@@ -10,6 +10,6 @@ info = f.get_meta_information()
 bounds = np.array(info['bounds'])
 
 # Start Bayesian optimization to optimize the objective function
-results = bayesian_optimization(f, bounds[0], bounds[1])
+results = bayesian_optimization(f, bounds[:, 0], bounds[:, 1], num_iterations=50)
 print(results["x_opt"])
 print(results["f_opt"])
