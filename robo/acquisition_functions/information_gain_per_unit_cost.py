d225ff039c8251d59baa1bd0275f6092f775e695
Aaron Klein <kleinaa@cs.uni-freiburg.de>
2017-02-13 14:27:08 +0100

    increased mcmc chain

--- robo/acquisition_functions/information_gain_per_unit_cost.py
+++ robo/acquisition_functions/information_gain_per_unit_cost.py
@@ -126,15 +126,20 @@ class InformationGainPerUnitCost(InformationGain):
 
         self.sampling_acquisition.update(self.model)
 
-        restarts = np.random.uniform(low=lower,
-                                     high=upper,
-                                     size=(self.Nb, D))
-
-        sampler = emcee.EnsembleSampler(self.Nb, D,
+        for i in range(5):
+            restarts = np.random.uniform(low=lower,
+                                         high=upper,
+                                         size=(self.Nb, D))
+            sampler = emcee.EnsembleSampler(self.Nb, D,
                                         self.sampling_acquisition_wrapper)
 
-        self.zb, self.lmb, _ = sampler.run_mcmc(restarts, 20)
-
+            self.zb, self.lmb, _ = sampler.run_mcmc(restarts, 50)
+            if not np.any(np.isinf(self.lmb)):
+                break
+            else:
+                print("Infinity")
+        if np.any(np.isinf(self.lmb)):
+            raise ValueError("Could not sample valid representer points! LogEI is -infinity")
         if len(self.zb.shape) == 1:
             self.zb = self.zb[:, None]
         if len(self.lmb.shape) == 1:
