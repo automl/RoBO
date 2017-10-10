6bbd6e38b3f6c3048a8b050e9ae818365dfdf177
Aaron Klein <kleinaa@cs.uni-freiburg.de>
2017-10-10 09:54:33 +0200

    restore original hyperband experiments

--- experiments/fabolas/run_hyperband.py
+++ experiments/fabolas/run_hyperband.py
@@ -6,7 +6,7 @@ import numpy as np
 
 logging.basicConfig(level=logging.INFO)
 
-from robo.solver.hyperband_datasets_size import HyperBand_DataSubsets
+from robo.solver.hyperband_datasets_size_original_incumbent import HyperBand_DataSubsetsOriginalIncumbent
 
 from hpolib.benchmarks.ml.svm_benchmark import SvmOnMnist, SvmOnVehicle, SvmOnCovertype, SvmOnAdult, SvmOnHiggs, SvmOnLetter
 from hpolib.benchmarks.ml.residual_networks import ResidualNeuralNetworkOnCIFAR10
@@ -21,31 +21,31 @@ rng = np.random.RandomState(seed)
 
 if dataset == "mnist":
     f = SvmOnMnist(rng=rng)
-    output_path = "./experiments/fabolas/results/svm_%s/hyperband_new_inc_%d" % (dataset, run_id)
+    output_path = "./experiments/fabolas/results/svm_%s/hyperband_%d" % (dataset, run_id)
 elif dataset == "vehicle":
     f = SvmOnVehicle(rng=rng)
-    output_path = "./experiments/fabolas/results/svm_%s/hyperband_new_inc_%d" % (dataset, run_id)
+    output_path = "./experiments/fabolas/results/svm_%s/hyperband_%d" % (dataset, run_id)
 elif dataset == "covertype":
     f = SvmOnCovertype(rng=rng)
-    output_path = "./experiments/fabolas/results/svm_%s/hyperband_new_inc_%d" % (dataset, run_id)
+    output_path = "./experiments/fabolas/results/svm_%s/hyperband_%d" % (dataset, run_id)
 elif dataset == "higgs":
     f = SvmOnHiggs(rng=rng)
-    output_path = "./experiments/fabolas/results/svm_%s/hyperband_new_inc_%d" % (dataset, run_id)
+    output_path = "./experiments/fabolas/results/svm_%s/hyperband_%d" % (dataset, run_id)
 elif dataset == "adult":
     f = SvmOnAdult(rng=rng)
-    output_path = "./experiments/fabolas/results/svm_%s/hyperband_new_inc_%d" % (dataset, run_id)
+    output_path = "./experiments/fabolas/results/svm_%s/hyperband_%d" % (dataset, run_id)
 elif dataset == "letter":
     f = SvmOnLetter(rng=rng)
-    output_path = "./experiments/fabolas/results/svm_%s/hyperband_new_inc_%d" % (dataset, run_id)
+    output_path = "./experiments/fabolas/results/svm_%s/hyperband_%d" % (dataset, run_id)
 elif dataset == "cifar10":
     f = ConvolutionalNeuralNetworkOnCIFAR10(rng=rng)
-    output_path = "./experiments/fabolas/results/cnn_%s/hyperband_new_inc_%d" % (dataset, run_id)
+    output_path = "./experiments/fabolas/results/cnn_%s/hyperband_%d" % (dataset, run_id)
 elif dataset == "svhn":
     f = ConvolutionalNeuralNetworkOnSVHN(rng=rng)
-    output_path = "./experiments/fabolas/results/cnn_%s/hyperband_new_inc_%d" % (dataset, run_id)
+    output_path = "./experiments/fabolas/results/cnn_%s/hyperband_%d" % (dataset, run_id)
 elif dataset == "res_net":
     f = ResidualNeuralNetworkOnCIFAR10(rng=rng)
-    output_path = "./experiments/fabolas/results/res_%s/hyperband_new_inc_%d" % (dataset, run_id)
+    output_path = "./experiments/fabolas/results/%s/hyperband_%d" % (dataset, run_id)
 
 
 os.makedirs(output_path, exist_ok=True)
@@ -55,9 +55,12 @@ B = -int(np.log(f.s_min)/np.log(3))
 
 print(B)
 
-opt = HyperBand_DataSubsets(f, eta, eta**(-(B-1)), output_path=output_path, rng=rng)
+opt = HyperBand_DataSubsetsOriginalIncumbent(f, eta, eta**(-(B-1)), output_path=output_path, rng=rng)
 
-opt.run(int(20 / B * 1.5))
+if dataset == "res_net":
+    opt.run(2)
+else:
+    opt.run(int(20 / B * 1.5))
 
 test_error = []
 for c in opt.incumbents:
