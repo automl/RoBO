9eeb9c82af4fbb84c9972f4b2943a045a0259a94
Aaron Klein <kleinaa@cs.uni-freiburg.de>
2017-11-16 14:34:31 +0100

    fix unittest

--- test/test_maximizer/test_maximizers_one_dim.py
+++ test/test_maximizer/test_maximizers_one_dim.py
@@ -6,11 +6,22 @@ from robo.maximizers.direct import Direct
 from robo.maximizers.grid_search import GridSearch
 from robo.maximizers.random_sampling import RandomSampling
 from robo.maximizers.scipy_optimizer import SciPyOptimizer
+from robo.acquisition_functions.base_acquisition import BaseAcquisitionFunction
+from test.dummy_model import DemoQuadraticModel
 
 
-def objective_function(x):
-    y = (0.5 - x) ** 2
-    return y
+class DemoAcquisitionFunction(BaseAcquisitionFunction):
+
+    def __init__(self):
+        model = DemoQuadraticModel()
+        X = np.random.rand(10, 1)
+        y = X ** 2
+        model.train(X, y[:, 0])
+        super(DemoAcquisitionFunction, self).__init__(model)
+
+    def compute(self, x, **kwargs):
+        y = (0.5 - x) ** 2
+        return np.array([y])
 
 
 class TestMaximizers1D(unittest.TestCase):
@@ -19,9 +30,10 @@ class TestMaximizers1D(unittest.TestCase):
 
         self.lower = np.array([0])
         self.upper = np.array([1])
+        self.objective_function = DemoAcquisitionFunction()
 
     def test_direct(self):
-        maximizer = Direct(objective_function, self.lower, self.upper)
+        maximizer = Direct(self.objective_function, self.lower, self.upper)
         x = maximizer.maximize()
 
         assert x.shape[0] == 1
@@ -30,7 +42,7 @@ class TestMaximizers1D(unittest.TestCase):
         assert np.all(x <= self.upper)
 
     def test_grid_search(self):
-        maximizer = GridSearch(objective_function, self.lower, self.upper)
+        maximizer = GridSearch(self.objective_function, self.lower, self.upper)
         x = maximizer.maximize()
 
         assert x.shape[0] == 1
@@ -39,7 +51,7 @@ class TestMaximizers1D(unittest.TestCase):
         assert np.all(x <= self.upper)
 
     def test_random_sampling(self):
-        maximizer = RandomSampling(objective_function, self.lower, self.upper)
+        maximizer = RandomSampling(self.objective_function, self.lower, self.upper)
         x = maximizer.maximize()
 
         assert x.shape[0] == 1
@@ -48,7 +60,7 @@ class TestMaximizers1D(unittest.TestCase):
         assert np.all(x <= self.upper)
 
     def test_scipy(self):
-        maximizer = SciPyOptimizer(objective_function, self.lower, self.upper)
+        maximizer = SciPyOptimizer(self.objective_function, self.lower, self.upper)
         x = maximizer.maximize()
 
         assert x.shape[0] == 1
--- test/test_maximizer/test_maximizers_two_dim.py
+++ test/test_maximizer/test_maximizers_two_dim.py
@@ -6,11 +6,22 @@ from robo.maximizers.cmaes import CMAES
 from robo.maximizers.direct import Direct
 from robo.maximizers.random_sampling import RandomSampling
 from robo.maximizers.scipy_optimizer import SciPyOptimizer
+from robo.acquisition_functions.base_acquisition import BaseAcquisitionFunction
+from test.dummy_model import DemoQuadraticModel
 
 
-def objective_function(x):
-    y = np.sum((0.5 - x) ** 2, axis=1)
-    return y
+class DemoAcquisitionFunction(BaseAcquisitionFunction):
+
+    def __init__(self):
+        model = DemoQuadraticModel()
+        X = np.random.rand(10, 2)
+        y = X ** 2
+        model.train(X, y[:, 0])
+        super(DemoAcquisitionFunction, self).__init__(model)
+
+    def compute(self, x, **kwargs):
+        y = np.sum((0.5 - x) ** 2, axis=1)
+        return np.array([y])
 
 
 class TestMaximizers2D(unittest.TestCase):
@@ -19,9 +30,10 @@ class TestMaximizers2D(unittest.TestCase):
 
         self.lower = np.array([0, 0])
         self.upper = np.array([1, 1])
+        self.objective_function = DemoAcquisitionFunction()
 
     def test_direct(self):
-        maximizer = Direct(objective_function, self.lower, self.upper)
+        maximizer = Direct(self.objective_function, self.lower, self.upper)
         x = maximizer.maximize()
 
         assert x.shape[0] == 2
@@ -30,7 +42,7 @@ class TestMaximizers2D(unittest.TestCase):
         assert np.all(x <= self.upper)
 
     def test_cmaes(self):
-        maximizer = CMAES(objective_function, self.lower, self.upper)
+        maximizer = CMAES(self.objective_function, self.lower, self.upper)
         x = maximizer.maximize()
 
         assert x.shape[0] == 2
@@ -39,7 +51,7 @@ class TestMaximizers2D(unittest.TestCase):
         assert np.all(x <= self.upper)
 
     def test_random_sampling(self):
-        maximizer = RandomSampling(objective_function, self.lower, self.upper)
+        maximizer = RandomSampling(self.objective_function, self.lower, self.upper)
         x = maximizer.maximize()
 
         assert x.shape[0] == 2
@@ -48,7 +60,7 @@ class TestMaximizers2D(unittest.TestCase):
         assert np.all(x <= self.upper)
 
     def test_scipy(self):
-        maximizer = SciPyOptimizer(objective_function, self.lower, self.upper)
+        maximizer = SciPyOptimizer(self.objective_function, self.lower, self.upper)
         x = maximizer.maximize()
 
         assert x.shape[0] == 2
