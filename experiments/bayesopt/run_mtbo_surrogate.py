fe59e21e3477ba5f9cfdaaf9b994ba1a1112e8dc
Aaron Klein <kleinaa@cs.uni-freiburg.de>
2017-12-22 15:32:14 +0100

    fix

--- experiments/bayesopt/run_mtbo_surrogate.py
+++ experiments/bayesopt/run_mtbo_surrogate.py
@@ -9,17 +9,25 @@ logging.basicConfig(level=logging.INFO)
 from robo.fmin import mtbo
 
 from hpolib.benchmarks.ml.surrogate_svm import SurrogateSVM
+from hpolib.benchmarks.ml.surrogate_cnn import SurrogateCNN
+from hpolib.benchmarks.ml.surrogate_fcnet import SurrogateFCNet
+
 
 run_id = int(sys.argv[1])
-seed = int(sys.argv[2])
+benchmark = sys.argv[2]
 auxillay_dataset = int(sys.argv[3])
-dataset = "surrogate"
 
-rng = np.random.RandomState(seed)
+rng = np.random.RandomState(run_id)
 
-f = SurrogateSVM(path="/home/kleinaa/experiments/fabolas/dataset/svm_on_mnist_grid", rng=rng)
+if benchmark == "svm_mnist":
+    f = SurrogateSVM(path="/ihome/kleinaa/devel/git/HPOlib/surrogates/")
+elif benchmark == "cnn_cifar10":
+    f = SurrogateCNN(path="/ihome/kleinaa/devel/git/HPOlib/surrogates/")
+elif benchmark == "fcnet_mnist":
+    f = SurrogateFCNet(path="/ihome/kleinaa/devel/git/HPOlib/surrogates/")
+
 num_iterations = 80
-output_path = "./experiments/fabolas_journal/results/svm_%s/mtbo_%d_%d" % (dataset, auxillay_dataset, run_id)
+output_path = "./experiments/RoBO/surrogate/"
 
 os.makedirs(output_path, exist_ok=True)
 
@@ -37,8 +45,8 @@ info = f.get_meta_information()
 bounds = np.array(info['bounds'])
 results = mtbo(objective_function=objective,
                lower=bounds[:, 0], upper=bounds[:, 1],
-               n_init=5, num_iterations=num_iterations, n_hypers=50,
-               rng=rng, output_path=output_path, inc_estimation="last_seen")
+               n_init=5, num_iterations=num_iterations,
+               n_hypers=50,rng=rng)
 
 results["run_id"] = run_id
 results['X'] = results['X'].tolist()
@@ -51,7 +59,7 @@ current_inc_val = None
 cum_cost = 0
 
 for i, inc in enumerate(results["incumbents"]):
-    print(inc)
+
     if current_inc == inc:
         test_error.append(current_inc_val)
     else:
@@ -60,7 +68,6 @@ for i, inc in enumerate(results["incumbents"]):
 
         current_inc = inc
         current_inc_val = y
-    print(current_inc_val)
 
     # Compute the time it would have taken to evaluate this configuration
     c = results["c"][i]
@@ -70,5 +77,9 @@ for i, inc in enumerate(results["incumbents"]):
     results["runtime"][i] += cum_cost
     results["test_error"] = test_error
 
-    with open(os.path.join(output_path, 'results_%d.json' % run_id), 'w') as fh:
-        json.dump(results, fh)
+p = os.path.join(output_path, benchmark, "mtbo_%d" % auxillay_dataset)
+os.makedirs(p, exist_ok=True)
+
+fh = open(os.path.join(p, '%s_run_%d.json' % (benchmark, run_id)), 'w')
+json.dump(results, fh)
+fh.close()
