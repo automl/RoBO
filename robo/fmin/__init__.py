37255228884cf3ea112b5e373c0b6a1722b7e26f
Aaron Klein <kleinaa@cs.uni-freiburg.de>
2018-11-12 09:44:44 +0100

    update inits

--- robo/fmin/__init__.py
+++ robo/fmin/__init__.py
@@ -1,29 +1,6 @@
-try:
-    from .bayesian_optimization import bayesian_optimization
-except ImportError:
-    pass
-try:
-    from .random_search import random_search
-except ImportError:
-    pass
-try:
-    from .fabolas import fabolas
-except ImportError:
-    pass
-try:
-    from .mtbo import mtbo
-except ImportError:
-    pass
-try:
-    from .bohamiann import bohamiann
-except ImportError:
-    pass
-
-try:
-    from .entropy_search import entropy_search
-except ImportError:
-    pass
-try:
-    from .warmstart_mtbo import warmstart_mtbo
-except ImportError:
-    pass
+from .bayesian_optimization import bayesian_optimization
+from .random_search import random_search
+from .fabolas import fabolas
+from .mtbo import mtbo
+from .entropy_search import entropy_search
+from .warmstart_mtbo import warmstart_mtbo
